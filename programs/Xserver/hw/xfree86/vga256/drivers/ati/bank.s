868fed09d1a8ee6f47313395be56a723ee2e2046
dawes <dawes>
1994-06-18 16:26:10 +0000

    new ati driver

--- programs/Xserver/hw/xfree86/vga256/drivers/ati/bank.s
+++ programs/Xserver/hw/xfree86/vga256/drivers/ati/bank.s
@@ -1,4 +1,4 @@
-/* $XConsortium: bank.s,v 1.2 94/03/29 11:08:08 dpw Exp $ */
+/* $XFree86: xc/programs/Xserver/hw/xfree86/vga256/drivers/ati/bank.s,v 2.0tsi 1994.06.07 Exp $ */
 /*
  * Copyright 1990,91 by Thomas Roell, Dinkelscherben, Germany.
  *
@@ -35,9 +35,12 @@
  * by Doug Evans, dje@sspiff.UUCP.
  * ALL DISCLAIMERS APPLY TO MY ADDITIONS AS WELL.
  *
- *	I've come to believe the people who design these cards couldn't
- *	allow for future enhancements if their life depended on it!
- *	The register set architecture is a joke!
+ * Changes to enhance support for the old V3 boards
+ * by Marc Aurele La France (TSI @ UQV), tsi@gpu.srv.ualberta.ca
+ * ALL DISCLAIMERS APPLY TO THESE CHANGES ALSO.
+ * 
+ * V3 boards use a 18800 chip and are single-banked.  Bank selection is done
+ * with bits 1-4 of extended register 1CE, index B2.
  *
  * Boards V4 and V5 have the 18800-1 chip. Boards Plus and XL have the 28800
  * chip. Page selection is done with Extended Register 1CE, Index B2.
@@ -45,18 +48,10 @@
  *
  * D7-D5 = Read page select bits 2-0
  * D4    = Reserved (18800-1)
- * D4    = Read page select bit 3 (28800)	Arrggghhhh!!!!
+ * D4    = Page select bit 3 (28800)
  * D3-D1 = Page select bits 2-0
  * D0    = Reserved (18800-1)
- * D0    = Page select bit 3 (28800)		Arrggghhhh!!!!
- *
- * Actually, it's even worse than this. The manual can't make up it's mind
- * whether its:
- *		R2 R1 R0 R3 W2 W1 W0 W3
- * or
- *		R2 R1 R0 W3 W2 W1 W0 R3
- *
- * It appears that the format is: R2 R1 R0 W3 W2 W1 W0 R3.
+ * D0    = Read page select bit 3 (28800)
  */
 
 #include "assyntax.h"
@@ -72,7 +67,9 @@
 
 /*
  * We have a mirror for the segment register because an I/O read costs so much
- * more time, that is better to keep the value of it in memory.
+ * more time, that is better to keep the value of it in memory.  However, this
+ * won't do for a V3 board because there are other bits in the segment select
+ * register to worry about.
  */
 
 Segment:
@@ -88,7 +85,7 @@ Segment:
 	ALIGNTEXT4
 	GLOBL	GLNAME(ATISetRead)
 GLNAME(ATISetRead):
-	AND_L	(CONST(0x0F),EAX)			/* FIXME: necessary? */
+	AND_L	(CONST(0x0F),EAX)
 	MOV_B	(CONTENT(Segment),AH)
 	AND_B	(CONST(0x1E),AH)
 	ROR_B	(CONST(3),AL)
@@ -96,13 +93,13 @@ GLNAME(ATISetRead):
 	MOV_B	(AH,CONTENT(Segment))
 	MOV_W	(CONTENT(GLNAME(ATIExtReg)),DX)
   	MOV_B	(CONST(0xB2),AL)
-	OUT1_W	(DX)
+	OUT_W
 	RET
 
         ALIGNTEXT4
 	GLOBL	GLNAME(ATISetWrite)
 GLNAME(ATISetWrite):
-	AND_L	(CONST(0x0F),EAX)			/* FIXME: necessary? */
+	AND_L	(CONST(0x0F),EAX)
 	MOV_B	(CONTENT(Segment),AH)
 	AND_B	(CONST(0xE1),AH)
 	SHL_B	(CONST(1),AL)
@@ -110,13 +107,13 @@ GLNAME(ATISetWrite):
 	MOV_B	(AH,CONTENT(Segment))
 	MOV_W	(CONTENT(GLNAME(ATIExtReg)),DX)
   	MOV_B	(CONST(0xB2),AL)
-	OUT1_W	(DX)
+	OUT_W
 	RET
 	
 	ALIGNTEXT4
 	GLOBL	GLNAME(ATISetReadWrite)
 GLNAME(ATISetReadWrite):
-	AND_L	(CONST(0x0F),EAX)			/* FIXME: necessary? */
+	AND_L	(CONST(0x0F),EAX)
 	MOV_B	(AL,AH)
 	SHL_B	(CONST(1),AH)
 	ROR_B	(CONST(3),AL)
@@ -124,5 +121,27 @@ GLNAME(ATISetReadWrite):
         MOV_B   (AH,CONTENT(Segment))
 	MOV_W	(CONTENT(GLNAME(ATIExtReg)),DX)
   	MOV_B	(CONST(0xB2),AL)
-	OUT1_W	(DX)
+	OUT_W
+	RET
+
+	ALIGNTEXT4
+	GLOBL	GLNAME(ATIV3SetRead)
+	GLOBL	GLNAME(ATIV3SetWrite)
+	GLOBL	GLNAME(ATIV3SetReadWrite)
+GLNAME(ATIV3SetRead):
+GLNAME(ATIV3SetWrite):
+GLNAME(ATIV3SetReadWrite):
+	AND_L	(CONST(0x0F),EAX)
+	SHL_B	(CONST(1),AL)
+	MOV_B	(AL,AH)
+	MOV_B	(CONST(0xB2),AL)
+	MOV_W	(CONTENT(GLNAME(ATIExtReg)),DX)
+	OUT_B
+	INC_W	(DX)
+	IN_B
+	AND_B	(CONST(0xE1),AL)
+	OR_B	(AL,AH)
+	DEC_W	(DX)
+	MOV_B	(CONST(0xB2),AL)
+	OUT_W
 	RET
