f5460a2d392b55f2158daaec67443822055b2874
dawes <dawes>
1994-06-22 04:11:06 +0000

    updates

--- programs/Xserver/hw/xfree86/xf86Version.h
+++ programs/Xserver/hw/xfree86/xf86Version.h
@@ -1,4 +1,4 @@
 /* $XConsortium: xf86Version.h,v 1.1 94/03/28 21:01:50 dpw Exp $ */
-/* $XFree86: xc/programs/Xserver/hw/xfree86/xf86Version.h,v 3.10 1994/06/12 16:35:07 dawes Exp $ */
+/* $XFree86: xc/programs/Xserver/hw/xfree86/xf86Version.h,v 3.11 1994/06/18 16:22:21 dawes Exp $ */
 
-#define XF86_VERSION " 3.0Bc "
+#define XF86_VERSION " 3.0C "
