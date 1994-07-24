07c54e4d32f0a7b8a0d6e2b2cacf9e5ba6f7277f
dawes <dawes>
1994-07-24 11:51:35 +0000

    updates

--- programs/Xserver/hw/xfree86/os-support/misc/xf86_IlHack.c
+++ programs/Xserver/hw/xfree86/os-support/misc/xf86_IlHack.c
@@ -1,7 +1,10 @@
 /* $XConsortium: xf86_IlHack.c,v 1.1 94/03/28 21:30:08 dpw Exp $ */
+/* $XFree86$ */
 /*
  * This file is an incredible crock to get the normally-inline functions
  * built into the server so that things can be debugged properly.
+ *
+ * Note: this doesn't work when using a compiler other than GCC.
  */
 
 
