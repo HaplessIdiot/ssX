cb1bd52111448bbed801341da81bc215ad8212e6
dawes <dawes>
1994-11-19 13:16:29 +0000

    updates

--- programs/Xserver/hw/xfree86/accel/mach32/mach32.h
+++ programs/Xserver/hw/xfree86/accel/mach32/mach32.h
@@ -1,5 +1,5 @@
 /* $XConsortium: mach32.h,v 1.1 94/03/28 21:06:49 dpw Exp $ */
-/* $XFree86: xc/programs/Xserver/hw/xfree86/accel/mach32/mach32.h,v 3.4 1994/09/07 15:49:11 dawes Exp $ */
+/* $XFree86: xc/programs/Xserver/hw/xfree86/accel/mach32/mach32.h,v 3.5 1994/09/11 00:48:42 dawes Exp $ */
 /*
  * Copyright 1992,1993 by Kevin E. Martin, Chapel Hill, North Carolina.
  *
@@ -64,6 +64,7 @@ extern pointer vgaBase;
 extern Bool xf86VTSema;
 extern short mach32MaxX, mach32MaxY;
 extern short mach32VirtX, mach32VirtY;
+extern short mach32DisplayWidth;
 extern Bool mach32Use4MbAperture;
 extern Bool mach32DAC8Bit;
 
