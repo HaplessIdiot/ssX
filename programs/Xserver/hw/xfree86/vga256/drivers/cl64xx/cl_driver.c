86eb6fffa4b13d8be2bb636cf047008a9e619d30
dawes <dawes>
1995-01-12 08:51:29 +0000

    updates

--- programs/Xserver/hw/xfree86/vga256/drivers/cl64xx/cl_driver.c
+++ programs/Xserver/hw/xfree86/vga256/drivers/cl64xx/cl_driver.c
@@ -1,4 +1,4 @@
-/* $XFree86: xc/programs/Xserver/hw/xfree86/vga256/drivers/cl64xx/cl_driver.c,v 3.1 1994/12/25 12:35:28 dawes Exp $ */
+/* $XFree86: xc/programs/Xserver/hw/xfree86/vga256/drivers/cl64xx/cl_driver.c,v 3.2 1995/01/10 10:31:00 dawes Exp $ */
 /*
  * Stubs driver Copyright 1993 by David Wexelblat <dwex@goblin.org>
  *
@@ -909,10 +909,13 @@ DisplayModePtr mode;
 	 * we adapt them here so that the VGA module will initialise
 	 * the CRTC fields correctly
 	 */
-	mode->HTotal <<= 1;
-	mode->HDisplay <<= 1;
-	mode->HSyncStart <<= 1;
-	mode->HSyncEnd <<= 1;
+	if (!mode->CrtcHAdjusted) {
+		mode->CrtcHTotal <<= 1;
+		mode->CrtcHDisplay <<= 1;
+		mode->CrtcHSyncStart <<= 1;
+		mode->CrtcHSyncEnd <<= 1;
+		mode->CrtcHAdjusted = TRUE;
+	}
 #endif
 
 	/*
@@ -932,30 +935,20 @@ DisplayModePtr mode;
 	 */
 	new->HBEX =
 		(new->std.CRTC[3] & 0x1F) |
-		((((mode->HSyncStart >> 3) - 1) & 0x100) >> 1);
+		((((mode->CrtcHSyncStart >> 3) - 1) & 0x100) >> 1);
 	new->HREX =
 		(new->std.CRTC[5] & 0x9F) |
-		(((mode->HSyncStart >> 3) & 0x100) >> 2) |
-		((((mode->HTotal >> 3) - 5) & 0x100) >> 3);
-
-#if !defined(MONOVGA) && !defined(XF86VGA16)
-	/*
-	 * Restore changed horizontal display parameters
-	 */
-	mode->HTotal >>= 1;
-	mode->HDisplay >>= 1;
-	mode->HSyncStart >>= 1;
-	mode->HSyncEnd >>= 1;
-#endif
+		(((mode->CrtcHSyncStart >> 3) & 0x100) >> 2) |
+		((((mode->CrtcHTotal >> 3) - 5) & 0x100) >> 3);
 
 	/*
 	 * For the vertical values only the extension bits have to be set
 	 */
 	new->VOVFL = 
-		(((mode->VTotal - 2) & 0x400) >> 10) |
-		(((mode->VDisplay - 1) & 0x400) >> 9) |
-		((mode->VSyncStart & 0x600) >> 7) |
-		((mode->VSyncStart & 0x400) >> 6);
+		(((mode->CrtcVTotal - 2) & 0x400) >> 10) |
+		(((mode->CrtcVDisplay - 1) & 0x400) >> 9) |
+		((mode->CrtcVSyncStart & 0x600) >> 7) |
+		((mode->CrtcVSyncStart & 0x400) >> 6);
 
 	new->std.CRTC[19] = vga256InfoRec.virtualX >> 3;
 	new->std.CRTC[23] = 0xE3;
@@ -1100,7 +1093,7 @@ int x, y;
 	 * will have additional bits in their extended registers, which
 	 * must also be set.
 	 */
-	int Base = (y * vga256InfoRec.virtualX + x), msb, tempin;
+	int Base = (y * vga256InfoRec.displayWidth + x), msb, tempin;
 
 #ifdef MONOVGA
   	msb = (Base + 3) >> 3;
--- programs/Xserver/hw/xfree86/xf86Version.h
+++ programs/Xserver/hw/xfree86/xf86Version.h
@@ -1,4 +1,4 @@
 /* $XConsortium: xf86Version.h,v 1.1 94/03/28 21:01:50 dpw Exp $ */
-/* $XFree86: xc/programs/Xserver/hw/xfree86/xf86Version.h,v 3.57 1995/01/10 10:20:12 dawes Exp $ */
+/* $XFree86: xc/programs/Xserver/hw/xfree86/xf86Version.h,v 3.58 1995/01/11 09:47:19 dawes Exp $ */
 
-#define XF86_VERSION " 3.1.0Bc "
+#define XF86_VERSION " 3.1.0Bd "
