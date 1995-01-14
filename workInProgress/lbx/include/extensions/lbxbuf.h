95edddb904330c31ee7ee2367a1907507994b98b
root <root>
1995-01-14 03:06:56 +0000

    Initial revision

---
+++ workInProgress/lbx/include/extensions/lbxbuf.h
@@ -0,0 +1,28 @@
+/* $XConsortium: lbxbuf.h,v 1.4 94/12/01 20:07:54 mor Exp $ */
+/*
+ * Copyright 1988-1993 Network Computing Devices, Inc.  All rights reserved.
+ * An unpublished work.
+ * 
+ * $NCDId: @(#)lbxbuf.h,v 1.1 1994/02/16 18:57:32 lemke Exp $
+ */
+
+#ifndef _BUFFER_H_
+#define _BUFFER_H_
+
+#define INBUFFER_SIZE		(1 << 13)	/* 8K */
+#define OUTBUFFER_SIZE		(1 << 12)	/* 4K */
+
+#ifndef TRUE
+#define TRUE    1
+#endif
+#ifndef FALSE
+#define FALSE   0
+#endif
+
+typedef struct _lzwbuffer  *LzwBufferPtr;
+
+extern int  InitLzwBuffer();
+extern void FreeLzwBuffer();
+extern char *ReserveOutBuf();
+
+#endif	/* _BUFFER_H_ */
---
+++ workInProgress/lbx/include/extensions/lbxbufstr.h
@@ -0,0 +1,26 @@
+/* $XConsortium: lbxbufstr.h,v 1.5 94/12/01 20:07:54 mor Exp $ */
+/*
+ * Copyright 1988-1993 Network Computing Devices, Inc.  All rights reserved.
+ * An unpublished work.
+ * 
+ * $NCDId: @(#)lbxbufstr.h,v 1.1 1994/02/16 18:57:42 lemke Exp $
+ */
+#ifndef _BUFFERSTR_H_
+#define _BUFFERSTR_H_
+
+#include	"lbxbuf.h"
+
+/*
+ * LZW Input/Output buffer
+ */
+typedef struct _lzwbuffer {
+	char 	*bufbase;
+	char	*bufend;
+	char	*bufptr;
+	int  	bufcnt;
+} LzwBuffer;
+
+#define BYTES_AVAIL(inbuf, len)	\
+	(((inbuf)->bufcnt >= (len)) ? (inbuf)->bufptr : NULL)
+
+#endif	/* _BUFFERSTR_H_ */
