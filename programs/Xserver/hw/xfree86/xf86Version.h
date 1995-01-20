93076e0d90aca411020427702e9a5005ff2e6e17
dawes <dawes>
1995-01-20 04:57:13 +0000

    updates

--- programs/Xserver/hw/xfree86/xf86Version.h
+++ programs/Xserver/hw/xfree86/xf86Version.h
@@ -1,4 +1,4 @@
 /* $XConsortium: xf86Version.h,v 1.1 94/03/28 21:01:50 dpw Exp $ */
-/* $XFree86: xc/programs/Xserver/hw/xfree86/xf86Version.h,v 3.63 1995/01/18 06:06:02 dawes Exp $ */
+/* $XFree86: xc/programs/Xserver/hw/xfree86/xf86Version.h,v 3.64 1995/01/19 05:45:17 dawes Exp $ */
 
-#define XF86_VERSION " 3.1.0Cb "
+#define XF86_VERSION " 3.1.0Cc "
