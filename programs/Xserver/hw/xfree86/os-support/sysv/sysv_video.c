4e66dea08b4534cb0f83ebe861447dcc3655aac6
dawes <dawes>
1995-03-08 10:17:39 +0000

    updates

--- programs/Xserver/hw/xfree86/os-support/sysv/sysv_video.c
+++ programs/Xserver/hw/xfree86/os-support/sysv/sysv_video.c
@@ -1,4 +1,5 @@
 /* $XConsortium: sysv_video.c,v 1.1 94/03/28 21:33:15 dpw Exp $ */
+/* $XFree86$ */
 /*
  * Copyright 1990,91 by Thomas Roell, Dinkelscherben, Germany
  * Copyright 1993 by David Wexelblat <dwex@goblin.org>
