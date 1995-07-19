19711f3979f625249111015cea237a23d5189fc5
dawes <dawes>
1995-07-19 13:01:48 +0000

    updates

--- programs/Xserver/hw/xfree86/xf86Version.h
+++ programs/Xserver/hw/xfree86/xf86Version.h
@@ -1,14 +1,14 @@
 /* $XConsortium: xf86Version.h,v 1.6 95/01/23 15:33:26 kaleb Exp $ */
-/* $XFree86: xc/programs/Xserver/hw/xfree86/xf86Version.h,v 3.101 1995/07/16 09:13:16 dawes Exp $ */
+/* $XFree86: xc/programs/Xserver/hw/xfree86/xf86Version.h,v 3.102 1995/07/17 14:15:56 dawes Exp $ */
 
-#define XF86_VERSION " 3.1.1Db "
+#define XF86_VERSION " 3.1.1Dc "
 
 /* The finer points in versions... */
 #define XF86_VERSION_MAJOR	3
 #define XF86_VERSION_MINOR	1
 #define XF86_VERSION_SUBMINOR	1
 #define XF86_VERSION_BETA	4	/* 0="", 1="A", 2="B", etc... */
-#define XF86_VERSION_ALPHA	2	/* 0="", 1="a", 2="b", etc... */
+#define XF86_VERSION_ALPHA	3	/* 0="", 1="a", 2="b", etc... */
 
 #define XF86_VERSION_NUMERIC(major,minor,subminor,beta,alpha)	\
    ((((((((major << 7) | minor) << 7) | subminor) << 5) | beta) << 5) | alpha)
