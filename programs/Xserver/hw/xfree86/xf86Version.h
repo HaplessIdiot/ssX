c445aac09a610c9cb8ff4e726b6c10ee1ed737ec
dawes <dawes>
1996-01-24 22:18:01 +0000

    updates

--- programs/Xserver/hw/xfree86/xf86Version.h
+++ programs/Xserver/hw/xfree86/xf86Version.h
@@ -1,14 +1,14 @@
 /* $XConsortium: xf86Version.h,v 1.6 95/01/23 15:33:26 kaleb Exp $ */
-/* $XFree86: xc/programs/Xserver/hw/xfree86/xf86Version.h,v 3.133 1996/01/17 12:46:43 dawes Exp $ */
+/* $XFree86: xc/programs/Xserver/hw/xfree86/xf86Version.h,v 3.134 1996/01/21 01:48:36 dawes Exp $ */
 
-#define XF86_VERSION " 3.1.2Bl "
+#define XF86_VERSION " 3.1.2Bm "
 
 /* The finer points in versions... */
 #define XF86_VERSION_MAJOR	3
 #define XF86_VERSION_MINOR	1
 #define XF86_VERSION_SUBMINOR	2
 #define XF86_VERSION_BETA	2	/* 0="", 1="A", 2="B", etc... */
-#define XF86_VERSION_ALPHA	12	/* 0="", 1="a", 2="b", etc... */
+#define XF86_VERSION_ALPHA	13	/* 0="", 1="a", 2="b", etc... */
 
 #define XF86_VERSION_NUMERIC(major,minor,subminor,beta,alpha)	\
    ((((((((major << 7) | minor) << 7) | subminor) << 5) | beta) << 5) | alpha)
@@ -19,4 +19,4 @@
 			XF86_VERSION_BETA,			\
 			XF86_VERSION_ALPHA)
 
-#define XF86_DATE	"Jan 21 1996"
+#define XF86_DATE	"Jan 25 1996"
