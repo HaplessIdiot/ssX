858219292e1f1333c1e99770328c97a1902109a4
dawes <dawes>
1996-08-19 13:36:44 +0000

    updates

--- programs/Xserver/hw/xfree86/xf86Version.h
+++ programs/Xserver/hw/xfree86/xf86Version.h
@@ -1,13 +1,13 @@
-/* $XFree86: xc/programs/Xserver/hw/xfree86/xf86Version.h,v 3.170 1996/08/16 12:28:25 dawes Exp $ */
+/* $XFree86: xc/programs/Xserver/hw/xfree86/xf86Version.h,v 3.171 1996/08/18 01:47:01 dawes Exp $ */
 
-#define XF86_VERSION " 3.1.2Ei "
+#define XF86_VERSION " 3.1.2Ej "
 
 /* The finer points in versions... */
 #define XF86_VERSION_MAJOR	3
 #define XF86_VERSION_MINOR	1
 #define XF86_VERSION_SUBMINOR	2
 #define XF86_VERSION_BETA	5	/* 0="", 1="A", 2="B", etc... */
-#define XF86_VERSION_ALPHA	9	/* 0="", 1="a", 2="b", etc... */
+#define XF86_VERSION_ALPHA	10	/* 0="", 1="a", 2="b", etc... */
 
 #define XF86_VERSION_NUMERIC(major,minor,subminor,beta,alpha)	\
    ((((((((major << 7) | minor) << 7) | subminor) << 5) | beta) << 5) | alpha)
@@ -18,6 +18,6 @@
 			XF86_VERSION_BETA,			\
 			XF86_VERSION_ALPHA)
 
-#define XF86_DATE	"Aug 18 1996"
+#define XF86_DATE	"Aug 19 1996"
 
 /* $XConsortium: xf86Version.h /main/36 1996/01/31 10:07:08 kaleb $ */
