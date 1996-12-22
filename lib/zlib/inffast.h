4084d4eae7dbd90d49f6459da52e5727abeab59d
dawes <dawes>
1996-12-22 03:28:24 +0000

    Initial revision

---
+++ lib/zlib/inffast.h
@@ -0,0 +1,19 @@
+/* $XConsortium: inffast.h /main/2 1996/03/07 13:53:25 mor $ */
+
+/* inffast.h -- header to use inffast.c
+ * Copyright (C) 1995-1996 Mark Adler
+ * For conditions of distribution and use, see copyright notice in zlib.h 
+ */
+
+/* WARNING: this file should *not* be used by applications. It is
+   part of the implementation of the compression library and is
+   subject to change. Applications should only use zlib.h.
+ */
+
+extern int inflate_fast OF((
+    uInt,
+    uInt,
+    inflate_huft *,
+    inflate_huft *,
+    inflate_blocks_statef *,
+    z_stream *));
