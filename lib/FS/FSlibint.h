fcedd876c3a2d3a2bfea16fce3d87795327b3af5
dawes <dawes>
1996-12-28 11:12:16 +0000

    updates

--- lib/FS/FSlibint.h
+++ lib/FS/FSlibint.h
@@ -51,6 +51,7 @@ used in advertising or otherwise to promote the sale, use or other dealings
 in this Software without prior written authorization from the X Consortium.
 
 */
+/* $XFree86$ */
 
 /*
  * FSlib internal decls
