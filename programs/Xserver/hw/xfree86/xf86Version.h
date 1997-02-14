b7aacc96f2dfa7eed57f0998f379e2e9686fd481
hohndel <hohndel>
1997-02-14 12:48:09 +0000

    update xf86Version.h to reflect 3.2Ad; npcheck /is/ useful...

--- programs/Xserver/hw/xfree86/xf86Version.h
+++ programs/Xserver/hw/xfree86/xf86Version.h
@@ -1,13 +1,13 @@
 /* $XFree86: xc/programs/Xserver/hw/xfree86/xf86Version.h,v 3.236 1997/01/27 06:57:29 dawes Exp $ */
 
-#define XF86_VERSION " 3.2Ac "
+#define XF86_VERSION " 3.2Ad "
 
 /* The finer points in versions... */
 #define XF86_VERSION_MAJOR	3
 #define XF86_VERSION_MINOR	2
 #define XF86_VERSION_SUBMINOR	0
 #define XF86_VERSION_BETA	1	/* 0="", 1="A", 2="B", etc... */
-#define XF86_VERSION_ALPHA	3	/* 0="", 1="a", 2="b", etc... */
+#define XF86_VERSION_ALPHA	4	/* 0="", 1="a", 2="b", etc... */
 
 #define XF86_VERSION_NUMERIC(major,minor,subminor,beta,alpha)	\
    ((((((((major << 7) | minor) << 7) | subminor) << 5) | beta) << 5) | alpha)
@@ -18,6 +18,6 @@
 			XF86_VERSION_BETA,			\
 			XF86_VERSION_ALPHA)
 
-#define XF86_DATE	"Feb 12 1997"
+#define XF86_DATE	"Feb 14 1997"
 
 /* $XConsortium: xf86Version.h /main/78 1996/10/28 05:42:10 kaleb $ */
