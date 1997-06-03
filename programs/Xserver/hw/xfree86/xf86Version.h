242352ee4021c59305397361839cb41bca41459e
hohndel <hohndel>
1997-06-03 14:54:08 +0000

    version date

--- programs/Xserver/hw/xfree86/xf86Version.h
+++ programs/Xserver/hw/xfree86/xf86Version.h
@@ -1,4 +1,4 @@
-/* $XFree86: xc/programs/Xserver/hw/xfree86/xf86Version.h,v 3.271 1997/05/09 09:19:40 hohndel Exp $ */
+/* $XFree86: xc/programs/Xserver/hw/xfree86/xf86Version.h,v 3.272 1997/05/11 13:47:02 dawes Exp $ */
 
 #define XF86_VERSION " 3.9c "
 
@@ -18,6 +18,6 @@
 			XF86_VERSION_BETA,			\
 			XF86_VERSION_ALPHA)
 
-#define XF86_DATE	"May xx 1997"
+#define XF86_DATE	"June  3 1997"
 
 /* $XConsortium: xf86Version.h /main/78 1996/10/28 05:42:10 kaleb $ */
