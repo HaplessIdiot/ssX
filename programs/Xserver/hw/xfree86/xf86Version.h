4b80b4771aeb509f204b18f061f684c8a74912c9
hohndel <hohndel>
1997-06-20 11:46:36 +0000

    preparing for 3.9g

--- programs/Xserver/hw/xfree86/xf86Version.h
+++ programs/Xserver/hw/xfree86/xf86Version.h
@@ -1,13 +1,13 @@
-/* $XFree86: xc/programs/Xserver/hw/xfree86/xf86Version.h,v 3.277 1997/06/15 07:12:15 dawes Exp $ */
+/* $XFree86: xc/programs/Xserver/hw/xfree86/xf86Version.h,v 3.278 1997/06/16 00:46:07 dawes Exp $ */
 
-#define XF86_VERSION " 3.9f "
+#define XF86_VERSION " 3.9g "
 
 /* The finer points in versions... */
 #define XF86_VERSION_MAJOR	3
 #define XF86_VERSION_MINOR	9
 #define XF86_VERSION_SUBMINOR	0
 #define XF86_VERSION_BETA	0	/* 0="", 1="A", 2="B", etc... */
-#define XF86_VERSION_ALPHA	4	/* 0="", 1="a", 2="b", etc... */
+#define XF86_VERSION_ALPHA	7	/* 0="", 1="a", 2="b", etc... */
 
 #define XF86_VERSION_NUMERIC(major,minor,subminor,beta,alpha)	\
    ((((((((major << 7) | minor) << 7) | subminor) << 5) | beta) << 5) | alpha)
@@ -18,6 +18,6 @@
 			XF86_VERSION_BETA,			\
 			XF86_VERSION_ALPHA)
 
-#define XF86_DATE	"June 16 1997"
+#define XF86_DATE	"June 20 1997"
 
 /* $XConsortium: xf86Version.h /main/78 1996/10/28 05:42:10 kaleb $ */
