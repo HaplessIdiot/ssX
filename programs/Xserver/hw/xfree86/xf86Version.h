962465e246defa7fba8b3d8a9ace2714014dcb22
dawes <dawes>
1997-06-30 06:31:09 +0000

    fix typo, and update date

--- programs/Xserver/hw/xfree86/xf86Version.h
+++ programs/Xserver/hw/xfree86/xf86Version.h
@@ -1,4 +1,4 @@
-/* $XFree86: xc/programs/Xserver/hw/xfree86/xf86Version.h,v 3.280 1997/06/22 10:16:57 dawes Exp $ */
+/* $XFree86: xc/programs/Xserver/hw/xfree86/xf86Version.h,v 3.281 1997/06/29 08:43:10 dawes Exp $ */
 
 #define XF86_VERSION " 3.9h "
 
@@ -18,6 +18,6 @@
 			XF86_VERSION_BETA,			\
 			XF86_VERSION_ALPHA)
 
-#define XF86_DATE	"June 29 1997"
+#define XF86_DATE	"June 30 1997"
 
 /* $XConsortium: xf86Version.h /main/78 1996/10/28 05:42:10 kaleb $ */
