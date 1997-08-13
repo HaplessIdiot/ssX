2039ff466bc9553f745a88aff1795c7144ce8f85
hohndel <hohndel>
1997-08-13 05:38:59 +0000

    preparing for 3.9m

--- programs/Xserver/hw/xfree86/xf86Version.h
+++ programs/Xserver/hw/xfree86/xf86Version.h
@@ -1,4 +1,4 @@
-/* $XFree86: xc/programs/Xserver/hw/xfree86/xf86Version.h,v 3.286 1997/07/31 07:16:06 dawes Exp $ */
+/* $XFree86: xc/programs/Xserver/hw/xfree86/xf86Version.h,v 3.287 1997/08/12 12:01:57 hohndel Exp $ */
 
 #define XF86_VERSION " 3.9m "
 
@@ -18,6 +18,6 @@
 			XF86_VERSION_BETA,			\
 			XF86_VERSION_ALPHA)
 
-#define XF86_DATE	"August 12 1997"
+#define XF86_DATE	"August 13 1997"
 
 /* $XConsortium: xf86Version.h /main/78 1996/10/28 05:42:10 kaleb $ */
