2eababe2a157b219c8e0eee073a10d55c7b86212
hohndel <hohndel>
1997-09-25 06:55:33 +0000

    lots of glint changes

---
+++ programs/Xserver/hw/xfree86/accel/glint/pm2_dac.c
@@ -0,0 +1,136 @@
+/* $XFree86: $ */
+/*
+ * Copyright 1997 The XFree86 Project, Inc
+ *
+ * programming the onboard RAMDAC of the Permedia 2 chip
+ *
+ * Dirk Hohndel <hohndel@XFree86.Org>
+ */
+
+#include "Xfuncproto.h"
+#include "compiler.h"
+#define NO_OSLIB_PROTOTYPES
+#include "xf86_OSlib.h"
+#include "xf86.h"
+#include "xf86_Config.h"
+
+#include "glint.h"
+#include "glint_regs.h"
+
+/*
+ * glintOutPM2DACIndReg() and glintInPM2DACIndReg() are used to access 
+ * the indirect DAC registers of the Permedia 2.
+ */
+void 
+glintOutPM2IndReg(unsigned char reg, unsigned char mask, unsigned char data)
+{
+    unsigned char tmp, tmp2 = 0x00;
+
+    GLINT_SLOW_WRITE_REG(reg, PM2DACIndexReg);
+
+    if (mask != 0x00)
+	tmp2 = GLINT_READ_REG(PM2DACIndexData) & mask;
+    GLINT_SLOW_WRITE_REG(tmp2 | data, PM2DACIndexData);
+}
+
+unsigned char 
+glintInPM2DACIndReg(unsigned char reg)
+{
+    unsigned char tmp, ret;
+
+    GLINT_SLOW_WRITE_REG(reg, PM2DACIndexReg);
+    ret = GLINT_READ_REG(PM2DACIndexData);
+
+    return(ret);
+}
+
+#define INITIALFREQERR 100000
+#define MINCLK 110000		/* VCO frequency range */
+#define MAXCLK 250000
+
+unsigned long
+PM2DAC_CalculateMNPCForClock
+(
+ unsigned long reqclock,		/* In kHz units */
+ unsigned long refclock,		/* In kHz units */
+ unsigned char *rm, 			/* M Out */
+ unsigned char *rn, 			/* N Out */
+ unsigned char *rp			/* P Out */
+ )
+{
+    unsigned char	m, n, p;
+    unsigned long	f;
+    long		freqerr, lowestfreqerr = INITIALFREQERR;
+    unsigned long  	clock,actualclock = 0;
+
+    for (n = 2; n <= 14; n++) {
+        for (m = 2; m != 0; m++) { /* this is a char, so this counts to 255 */
+	    f = refclock * m / n;
+	    if ( (f < MINCLK) || (f > MAXCLK) )
+	    	continue;
+	    for (p = 0; p <= 4; p++) {
+	    	clock = f >> p;
+		freqerr = reqclock - clock;
+		if (freqerr < 0)
+		    freqerr = -freqerr;
+		if (freqerr < lowestfreqerr) {
+		    *rn = n;
+		    *rm = m;
+		    *rp = p;
+		    lowestfreqerr = freqerr;
+		    actualclock = clock;
+#ifdef DEBUG
+	ErrorF("Best %ld diff %ld\n",actualclock,freqerr);
+#endif
+		}
+	    }
+	}
+    }
+
+    return(actualclock);
+}
+
+void 
+PM2DACGlintSetClock(long freq)
+{
+    unsigned char m,n,p;
+    unsigned long clockused;
+
+    unsigned long fref = 14318;
+    clockused = PM2DAC_CalculateMNPCForClock(freq,fref,&m,&n,&p);
+
+#ifdef DEBUG
+    ErrorF("requested %ld, setting to %ld, m %d, n %d, p %d\n",
+           freq,clockused,m,n,p);
+#endif
+
+    /*
+     * now set the clock
+     */
+    glintOutPM2IndReg(PM2DACIndexClockCM,0x00,m);
+    glintOutPM2IndReg(PM2DACIndexClockCN,0x00,n);
+    glintOutPM2IndReg(PM2DACIndexClockCP,0x00,p);
+}
+
+int
+PM2DACInit(int clock)
+{
+    /*
+     * set up the RAMDAC in the right mode
+     */
+    switch (glintInfoRec.depth)
+    {
+    case 8:
+    	glintOutPM2IndReg(PM2DACIndexCMR,0x00,
+			  PM2DAC_RGB|PM2DAC_GRAPHICS|PM2DAC_CI8);
+    	break;
+    case 15:
+    	break;
+    case 16:
+    	break;
+    	break;
+    case 32:
+    	break;
+    }
+    PM2DACGlintSetClock(clock);
+}
