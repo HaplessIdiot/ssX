dd8324001219bd1237bde8ec20419a346311c28e
robin <robin>
1998-06-05 14:32:46 +0000

    new version

--- programs/Xserver/hw/xfree86/xf86Version.h
+++ programs/Xserver/hw/xfree86/xf86Version.h
@@ -1,13 +1,13 @@
-/* $XFree86: xc/programs/Xserver/hw/xfree86/xf86Version.h,v 3.314 1998/04/27 03:14:52 robin Exp $ */
+/* $XFree86: xc/programs/Xserver/hw/xfree86/xf86Version.h,v 3.315 1998/04/28 12:48:43 robin Exp $ */
 
-#define XF86_VERSION " 3.9Ah "
+#define XF86_VERSION " 3.9Ai "
 
 /* The finer points in versions... */
 #define XF86_VERSION_MAJOR	3
 #define XF86_VERSION_MINOR	9
 #define XF86_VERSION_SUBMINOR	0
 #define XF86_VERSION_BETA	1	/* 0="", 1="A", 2="B", etc... */
-#define XF86_VERSION_ALPHA	8	/* 0="", 1="a", 2="b", etc... */
+#define XF86_VERSION_ALPHA	9	/* 0="", 1="a", 2="b", etc... */
 
 #define XF86_VERSION_NUMERIC(major,minor,subminor,beta,alpha)	\
    ((((((((major << 7) | minor) << 7) | subminor) << 5) | beta) << 5) | alpha)
@@ -18,6 +18,6 @@
 			XF86_VERSION_BETA,			\
 			XF86_VERSION_ALPHA)
 
-#define XF86_DATE	"April 28 1998"
+#define XF86_DATE	"June 5 1998"
 
 /* $XConsortium: xf86Version.h /main/78 1996/10/28 05:42:10 kaleb $ */
