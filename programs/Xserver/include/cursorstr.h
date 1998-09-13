769827b61753f7a4be29f1930eb1993f08a87d49
dawes <dawes>
1998-09-13 13:29:28 +0000

    missing ident line

--- programs/Xserver/include/cursorstr.h
+++ programs/Xserver/include/cursorstr.h
@@ -46,6 +46,8 @@ ARISING OUT OF OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS
 SOFTWARE.
 
 ******************************************************************/
+/* $XFree86$ */
+
 #ifndef CURSORSTRUCT_H
 #define CURSORSTRUCT_H 
 
