c9532d31818c2095146b9043558edea71125e088
dawes <dawes>
1998-10-04 14:35:53 +0000

    quick workaround for static build problem

--- programs/Xserver/hw/xfree86/drivers/glint/pm_accel.c
+++ programs/Xserver/hw/xfree86/drivers/glint/pm_accel.c
@@ -28,7 +28,7 @@
  * 
  * Permedia accelerated options.
  */
-/* $XFree86: xc/programs/Xserver/hw/xfree86/drivers/glint/pm_accel.c,v 1.4 1998/08/13 14:45:53 dawes Exp $ */
+/* $XFree86: xc/programs/Xserver/hw/xfree86/drivers/glint/pm_accel.c,v 1.5 1998/09/05 06:36:48 dawes Exp $ */
 
 #include "xf86.h"
 #include "xf86_OSproc.h"
@@ -912,9 +912,11 @@ PermediaNonTEGlyphRenderer(
 					pScrn, xText, y, wText, h, 0);
 
    	while(h--) {
+#if 0 /* XXX FIX THIS! */
 	    XAANonTEGlyphScanlineFuncLSBFirst(
 		(CARD32*)infoRec->ColorExpandBase, 
 		glyphp, startline++, wText, skipleft);
+#endif
 
     	}
     }
