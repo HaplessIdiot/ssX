79e5704b49b764f1086defd96d55909b277799b3
dawes <dawes>
1999-02-07 11:47:14 +0000

    missing ident lines

--- lib/font/Speedo/out_bl2d.c
+++ lib/font/Speedo/out_bl2d.c
@@ -21,6 +21,7 @@ INCIDENTAL OR CONSEQUENTIAL DAMAGES, ARISING OUT OF OR IN ANY WAY CONNECTED
 WITH THE SPEEDO SOFTWARE OR THE BITSTREAM CHARTER OUTLINE FONT.
 
 */
+/* $XFree86$ */
 
 /*************************** O U T _ B L 2 D . C *****************************
  *                                                                           *
