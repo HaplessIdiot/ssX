65ddec8612ac5389b9deff6577f618add02595d3
dawes <dawes>
1999-07-11 10:47:09 +0000

    remove the xfree68 and xfree98 directories, moving some files to hw/xfree86 remove old drivers from hw/xfree86

--- programs/Xserver/hw/xfree86/drivers/nv/nvcursor.h
+++
@@ -1,9 +0,0 @@
-/* $XFree86: xc/programs/Xserver/hw/xfree86/drivers/nv/nvcursor.h,v 1.2 1997/03/23 09:01:44 hohndel Exp $ */
-
-
-
-
-Bool NVCursorInit();
-void NVRestoreCursor();
-void NVWarpCursor();
-void NVQueryBestSize();
