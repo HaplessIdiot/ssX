c3e1d14eabc298dd306ea8a64dc9115042768e82
dawes <dawes>
1999-08-01 07:20:53 +0000

    ported nvidia driver

---
+++ programs/Xserver/hw/xfree86/drivers/nv/nv_dac.c
@@ -0,0 +1,210 @@
+/* $XConsortium: nv_driver.c /main/3 1996/10/28 05:13:37 kaleb $ */
+/*
+ * Copyright 1996-1997  David J. McKay
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a
+ * copy of this software and associated documentation files (the "Software"),
+ * to deal in the Software without restriction, including without limitation
+ * the rights to use, copy, modify, merge, publish, distribute, sublicense,
+ * and/or sell copies of the Software, and to permit persons to whom the
+ * Software is furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.  IN NO EVENT SHALL
+ * DAVID J. MCKAY BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
+ * WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF
+ * OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
+/* Hacked together from mga driver and 3.3.4 NVIDIA driver by Jarno Paananen
+   <jpaana@s2.org> */
+
+/* $XFree86: xc/programs/Xserver/hw/xfree86/drivers/nv/nv_driver.c,v 1.8 1998/01/24 16:58:08 hohndel Exp $ */
+
+#include "nv_include.h"
+
+#include "nvreg.h"
+#include "nvvga.h"
+
+Bool
+NVDACInit(ScrnInfoPtr pScrn, DisplayModePtr mode)
+{
+    int i;
+    int horizDisplay = (mode->CrtcHDisplay/8)   - 1;
+    int horizStart   = (mode->CrtcHSyncStart/8) - 1;
+    int horizEnd     = (mode->CrtcHSyncEnd/8)   - 1;
+    int horizTotal   = (mode->CrtcHTotal/8)     - 1;
+    int vertDisplay  =  mode->CrtcVDisplay      - 1;
+    int vertStart    =  mode->CrtcVSyncStart    - 1;
+    int vertEnd      =  mode->CrtcVSyncEnd      - 1;
+    int vertTotal    =  mode->CrtcVTotal        - 2;
+
+    NVPtr pNv = NVPTR(pScrn);
+    NVRegPtr nvReg = &pNv->ModeReg;
+    vgaRegPtr   pVga;
+    
+    DEBUG(xf86DrvMsg(pScrn->scrnIndex, X_INFO, "NVDACInit\n"));
+    
+    /*
+     * This will initialize all of the generic VGA registers.
+     */
+    if (!vgaHWInit(pScrn, mode))
+        return(FALSE);
+
+    pVga = &VGAHWPTR(pScrn)->ModeReg;
+
+    /*
+     * Set all CRTC values.
+     */
+
+    pVga->CRTC[0x0]  = Set8Bits(horizTotal - 4);
+    pVga->CRTC[0x1]  = Set8Bits(horizDisplay);
+    pVga->CRTC[0x2]  = Set8Bits(horizDisplay);
+    pVga->CRTC[0x3]  = SetBitField(horizTotal,4:0,4:0) 
+                                                 | SetBit(7);
+    pVga->CRTC[0x4]  = Set8Bits(horizStart);
+    pVga->CRTC[0x5]  = SetBitField(horizTotal,5:5,7:7)
+        | SetBitField(horizEnd,4:0,4:0);
+    pVga->CRTC[0x6]  = SetBitField(vertTotal,7:0,7:0);
+    pVga->CRTC[0x7]  = SetBitField(vertTotal,8:8,0:0)
+        | SetBitField(vertDisplay,8:8,1:1)
+        | SetBitField(vertStart,8:8,2:2)
+        | SetBitField(vertDisplay,8:8,3:3)
+        | SetBit(4)
+        | SetBitField(vertTotal,9:9,5:5)
+        | SetBitField(vertDisplay,9:9,6:6)
+        | SetBitField(vertStart,9:9,7:7);
+    pVga->CRTC[0x9]  = SetBitField(vertDisplay,9:9,5:5)
+        | SetBit(6)
+        | ((mode->Flags & V_DBLSCAN) ? 0x80 : 0x00);
+    pVga->CRTC[0x10] = Set8Bits(vertStart);
+    pVga->CRTC[0x11] = SetBitField(vertEnd,3:0,3:0) | SetBit(5);
+    pVga->CRTC[0x12] = Set8Bits(vertDisplay);
+    pVga->CRTC[0x13] = ((pScrn->displayWidth/8)*(pScrn->bitsPerPixel/8)) & 0xFF;
+    pVga->CRTC[0x15] = Set8Bits(vertDisplay);
+    pVga->CRTC[0x16] = Set8Bits(vertTotal + 1);
+    
+    /*
+     * Initialize DAC palette.
+     */
+    if(pScrn->bitsPerPixel != 8 )
+    {
+        if (pNv->riva.Architecture == 3)
+            for (i = 0; i < 256; i++)
+            {
+                pVga->DAC[i*3]     = i >> 2;
+                pVga->DAC[(i*3)+1] = i >> 2;
+                pVga->DAC[(i*3)+2] = i >> 2;
+            }
+        else
+            for (i = 0; i < 256; i++)
+            {
+                pVga->DAC[i*3]     = i;
+                pVga->DAC[(i*3)+1] = i;
+                pVga->DAC[(i*3)+2] = i;
+            }
+    }
+    /*
+     * Calculate the extended registers.
+     */
+    switch (pScrn->bitsPerPixel)
+    {
+        case 8:
+            i = 8;
+            break;
+        case 15:
+        case 16:
+            i = (pScrn->weight.green == 6) ? 16 : 15;
+            break;
+        default:
+            i = 32;
+    }
+
+    pNv->riva.CalcStateExt(&pNv->riva, 
+                           nvReg,
+                           i, /* BPP */
+                           pScrn->displayWidth,
+                           mode->CrtcHDisplay,
+                           horizDisplay,
+                           horizStart,
+                           horizEnd,
+                           horizTotal,
+                           pScrn->virtualY,
+                           vertDisplay,
+                           vertStart,
+                           vertEnd,
+                           vertTotal,
+                           mode->Clock);
+
+    return (TRUE);
+}
+
+void 
+NVDACRestore(ScrnInfoPtr pScrn, vgaRegPtr vgaReg, NVRegPtr nvReg,
+             Bool restoreFonts)
+{
+    NVPtr pNv = NVPTR(pScrn);
+    DEBUG(xf86DrvMsg(pScrn->scrnIndex, X_INFO, "NVDACRestore\n"));
+    vgaHWRestore(pScrn, vgaReg,
+                 VGA_SR_MODE | (restoreFonts? VGA_SR_FONTS : 0));
+    pNv->riva.LoadStateExt(&pNv->riva, nvReg);
+}
+
+/*
+ * NVRamdacInit
+ */
+void
+NVRamdacInit(ScrnInfoPtr pScrn)
+{
+    DEBUG(xf86DrvMsg(pScrn->scrnIndex, X_INFO, "NVRamdacInit\n"));
+}
+
+/*
+ * NVDACSave
+ *
+ * This function saves the video state.
+ */
+void
+NVDACSave(ScrnInfoPtr pScrn, vgaRegPtr vgaReg, NVRegPtr nvReg,
+          Bool saveFonts)
+{
+    NVPtr pNv = NVPTR(pScrn);
+    DEBUG(xf86DrvMsg(pScrn->scrnIndex, X_INFO, "NVDACSave\n"));
+    vgaHWSave(pScrn, vgaReg, VGA_SR_MODE | (saveFonts? VGA_SR_FONTS : 0));
+    pNv->riva.UnloadStateExt(&pNv->riva, nvReg);
+}
+
+void
+NVDACLoadPalette(ScrnInfoPtr pScrn, int numColors, int *indices, LOCO *colors,
+                 VisualPtr pVisual )
+{
+    NVPtr pNv = NVPTR(pScrn);
+    int i, index;
+    vgaRegPtr   pVga;
+
+    pVga = &VGAHWPTR(pScrn)->ModeReg;
+
+    DEBUG(xf86DrvMsg(pScrn->scrnIndex, X_INFO, "NVDACLoadPalette\n"));
+    for(i = 0; i < numColors; i++) {
+        index = indices[i];
+        if (pNv->riva.Architecture == 3)
+        {
+            pVga->DAC[index*3]     = colors[index].red >> 2;
+            pVga->DAC[(index*3)+1] = colors[index].green >> 2;
+            pVga->DAC[(index*3)+2] = colors[index].blue >> 2;
+        }
+        else
+        {
+            pVga->DAC[index*3]     = colors[index].red;
+            pVga->DAC[(index*3)+1] = colors[index].green;
+            pVga->DAC[(index*3)+2] = colors[index].blue;
+        }
+    }
+    vgaHWRestore(pScrn, pVga, VGA_SR_CMAP);
+}
+
---
+++ programs/Xserver/hw/xfree86/drivers/nv/nv_type.h
@@ -0,0 +1,134 @@
+#ifndef __NV_STRUCT_H__
+#define __NV_STRUCT_H__
+
+#include "riva_hw.h"
+
+#define SetBitField(value,from,to) SetBF(to, GetBF(value,from))
+#define SetBit(n) (1<<(n))
+#define Set8Bits(value) ((value)&0xff)
+
+
+typedef RIVA_HW_STATE* NVRegPtr;
+
+typedef struct {
+    Bool        isHwCursor;
+    int         CursorMaxWidth;
+    int         CursorMaxHeight;
+    int         CursorFlags;
+    int         CursorOffscreenMemSize;
+    Bool        (*UseHWCursor)(ScreenPtr, CursorPtr);
+    void        (*LoadCursorImage)(ScrnInfoPtr, unsigned char*);
+    void        (*ShowCursor)(ScrnInfoPtr);
+    void        (*HideCursor)(ScrnInfoPtr);
+    void        (*SetCursorPosition)(ScrnInfoPtr, int, int);
+    void        (*SetCursorColors)(ScrnInfoPtr, int, int);
+    long        maxPixelClock;
+    long        MemoryClock;
+    MessageType ClockFrom;
+    MessageType MemClkFrom;
+    Bool        SetMemClk;
+    void        (*LoadPalette)(ScrnInfoPtr, int, int*, LOCO*, VisualPtr);
+    void        (*PreInit)(ScrnInfoPtr);
+    void        (*Save)(ScrnInfoPtr, vgaRegPtr, NVRegPtr, Bool);
+    void        (*Restore)(ScrnInfoPtr, vgaRegPtr, NVRegPtr, Bool);
+    Bool        (*ModeInit)(ScrnInfoPtr, DisplayModePtr);
+} NVRamdacRec, *NVRamdacPtr;
+
+
+typedef struct {
+    RIVA_HW_INST        riva;
+    RIVA_HW_STATE       SavedReg;
+    RIVA_HW_STATE       ModeReg;
+    EntityInfoPtr       pEnt;
+    pciVideoPtr         PciInfo;
+    PCITAG              PciTag;
+    xf86AccessRec       Access;
+    int                 Chipset;
+    int                 ChipRev;
+    Bool                Primary;
+    Bool                Interleave;
+    int                 HwBpp;
+    int                 Rounding;
+    int                 BppShift;
+    Bool                HasFBitBlt;
+    Bool                OverclockMem;
+    int                 YDstOrg;
+    int                 DstOrg;
+    int                 SrcOrg;
+    CARD32              IOAddress;
+    CARD32              FbAddress;
+    CARD32              ILOADAddress;
+    int                 FbBaseReg;
+    unsigned char *     IOBase;
+#ifdef __alpha__
+    unsigned char *     IOBaseDense;
+#endif
+    unsigned char *     FbBase;
+    unsigned char *     FbStart;
+    long                FbMapSize;
+    long                FbUsableSize;
+    long                FbCursorOffset;
+    NVRamdacRec         Dac;
+    Bool                NoAccel;
+    Bool                SyncOnGreen;
+    Bool                Dac6Bit;
+    Bool                HWCursor;
+    Bool                UsePCIRetry;
+    Bool                ShowCache;
+    Bool                Overlay8Plus24;
+    Bool                ShadowFB;
+    unsigned char *     ShadowPtr;
+    int                 ShadowPitch;
+    int                 MemClk;
+    int                 MinClock;
+    int                 MaxClock;
+    int                 MaxFastBlitY;
+    CARD32              BltScanDirection;
+    CARD32              FilledRectCMD;
+    CARD32              SolidLineCMD;
+    CARD32              PatternRectCMD;
+    CARD32              DashCMD;
+    CARD32              NiceDashCMD;
+    CARD32              AccelFlags;
+    CARD32              PlaneMask;
+    CARD32              FgColor;
+    CARD32              BgColor;
+    int                 StyleLen;
+    XAAInfoRecPtr       AccelInfoRec;
+    xf86CursorInfoPtr   CursorInfoRec;
+    DGAModePtr          DGAModes;
+    int                 numDGAModes;
+    Bool                DGAactive;
+    int                 DGAViewportStatus;
+    CARD32              *Atype;
+    CARD32              *AtypeNoBLK;
+    void                (*PreInit)(ScrnInfoPtr pScrn);
+    void                (*Save)(ScrnInfoPtr, vgaRegPtr, NVRegPtr, Bool);
+    void                (*Restore)(ScrnInfoPtr, vgaRegPtr, NVRegPtr, Bool);
+    Bool                (*ModeInit)(ScrnInfoPtr, DisplayModePtr);
+    CloseScreenProcPtr  CloseScreen;
+/*    unsigned int        (*ddc1Read)(ScrnInfoPtr);
+    void (*DDC1SetSpeed)(ScrnInfoPtr, xf86ddcSpeed);
+    Bool                (*i2cInit)(ScrnInfoPtr);
+    I2CBusPtr           I2C;*/
+    Bool                FBDev;
+    int                 colorKey;
+} NVRec, *NVPtr;
+
+#define NVPTR(p) ((NVPtr)((p)->driverPrivate))
+
+
+typedef enum {
+    OPTION_SW_CURSOR,
+    OPTION_HW_CURSOR,
+    OPTION_PCI_RETRY,
+    OPTION_RGB_BITS,
+    OPTION_NOACCEL,
+    OPTION_SHOWCACHE,
+    OPTION_SHADOW_FB,
+    OPTION_FBDEV,
+    OPTION_COLOR_KEY,
+    OPTION_SET_MCLK
+} NVOpts;
+
+#endif /* __NV_STRUCT_H__ */
