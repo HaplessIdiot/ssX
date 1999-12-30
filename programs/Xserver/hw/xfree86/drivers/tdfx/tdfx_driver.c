6403c8d4d080475cd9d2c3a8c90f4a09d5bfd40b
robin <robin>
1999-12-30 03:25:24 +0000

    3519. Resolve missing symbol in TDFX driver (#3531, Dave Gilbert).

--- programs/Xserver/hw/xfree86/drivers/tdfx/tdfx_driver.c
+++ programs/Xserver/hw/xfree86/drivers/tdfx/tdfx_driver.c
@@ -25,7 +25,7 @@ TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE
 SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 
 **************************************************************************/
-/* $XFree86: xc/programs/Xserver/hw/xfree86/drivers/tdfx/tdfx_driver.c,v 1.7 1999/12/14 01:33:50 robin Exp $ */
+/* $XFree86: xc/programs/Xserver/hw/xfree86/drivers/tdfx/tdfx_driver.c,v 1.8 1999/12/27 00:39:45 robin Exp $ */
 
 /*
  * Authors:
@@ -239,6 +239,7 @@ static const char *xaaSymbols[] = {
     "XAAOverlayFBfuncs",
     "XAACachePlanarMonoStipple",
     "XAAScreenIndex",
+    "XAAReverseBitOrder",
     NULL
 };
 
