90cdb684f5d40f2899c9e7e63a59df2bda7abc8b
alanh <alanh>
2000-02-02 21:07:52 +0000

    Fix Cyber9388 detection.

--- programs/Xserver/hw/xfree86/drivers/trident/trident_driver.c
+++ programs/Xserver/hw/xfree86/drivers/trident/trident_driver.c
@@ -1366,6 +1366,21 @@ TRIDENTPreInit(ScrnInfoPtr pScrn, int flags)
 		    break;
 	    }
 	    break;
+	case CYBER9388:
+    	    pTrident->ddc1Read = Tridentddc1Read;
+    	    if ((INB(vgaIOBase + 5) & 0x0C) == 0x04)
+		ramtype = "EDO Ram";
+    	    if ((INB(vgaIOBase + 5) & 0x0C) == 0x08)
+		ramtype = "SDRAM";
+    	    if ((INB(vgaIOBase + 5) & 0x0C) == 0x0C) {
+		pTrident->HasSGRAM = TRUE;
+		ramtype = "SGRAM";
+	    }
+	    pTrident->IsCyber = TRUE;
+	    Support24bpp = TRUE;
+	    chipset = "Cyber 9388";
+	    pTrident->NewClockCode = TRUE;
+	    break;
 	case CYBER9397:
     	    pTrident->ddc1Read = Tridentddc1Read;
     	    if ((INB(vgaIOBase + 5) & 0x0C) == 0x04)
