08d1b882cd981d21075f3c629643e784c70ba133
mvojkovi <mvojkovi>
2000-02-11 06:31:48 +0000

    mioverlay layer for handling two window trees of different depths.

---
+++ programs/Xserver/mi/mioverlay.h
@@ -0,0 +1,25 @@
+#ifndef __MIOVERLAY_H
+#define __MIOVERLAY_H
+
+typedef void (*miOverlayTransFunc)(ScreenPtr, int, BoxPtr);
+
+Bool
+miInitOverlay(
+   ScreenPtr pScreen, 
+   int overlayDepth, 
+   miOverlayTransFunc trans
+);
+
+Bool
+miOverlayGetPrivateClips(
+    WindowPtr pWin,
+    RegionPtr *borderClip,
+    RegionPtr *clipList
+);
+
+Bool miOverlayCollectUnderlayRegions(WindowPtr, RegionPtr*);
+void miOverlayComputeCompositeClip(GCPtr, WindowPtr);
+Bool miOverlayCopyUnderlay(ScreenPtr);
+void miOverlaySetTransFunction(ScreenPtr, miOverlayTransFunc);
+
+#endif /* __MIOVERLAY_H */
