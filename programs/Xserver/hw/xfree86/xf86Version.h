350bd1dd21227aa8b39227b32a4136cf1e7504a1
dawes <dawes>
2000-02-14 18:11:44 +0000

    3764. DPS build fixes (#3686, Juliusz Chroboczek, David Dawes). 3763. Back out the pMga->PointerMoved change. Use /usr/bin/cpp instead of /usr/libexec/cpp on FreeBSD

--- programs/Xserver/hw/xfree86/xf86Version.h
+++ programs/Xserver/hw/xfree86/xf86Version.h
@@ -1,13 +1,13 @@
-/* $XFree86: xc/programs/Xserver/hw/xfree86/xf86Version.h,v 3.414 2000/02/12 20:45:09 dawes Exp $ */
+/* $XFree86: xc/programs/Xserver/hw/xfree86/xf86Version.h,v 3.415 2000/02/13 07:20:37 dawes Exp $ */
 
-#define XF86_VERSION " 3.9.17f "
+#define XF86_VERSION " 3.9.17Z "
 
 /* The finer points in versions... */
 #define XF86_VERSION_MAJOR	3
 #define XF86_VERSION_MINOR	9
-#define XF86_VERSION_SUBMINOR	17
+#define XF86_VERSION_SUBMINOR	18
 #define XF86_VERSION_BETA	0	/* 0="", 1="A", 2="B", etc... */
-#define XF86_VERSION_ALPHA	6	/* 0="", 1="a", 2="b", etc... */
+#define XF86_VERSION_ALPHA	0	/* 0="", 1="a", 2="b", etc... */
 
 #define XF86_VERSION_NUMERIC(major,minor,subminor,beta,alpha)	\
    ((((((((major << 7) | minor) << 7) | subminor) << 5) | beta) << 5) | alpha)
@@ -18,6 +18,6 @@
 			XF86_VERSION_BETA,			\
 			XF86_VERSION_ALPHA)
 
-#define XF86_DATE	"12 February 2000"
+#define XF86_DATE	"14 February 2000"
 
 /* $XConsortium: xf86Version.h /main/78 1996/10/28 05:42:10 kaleb $ */
