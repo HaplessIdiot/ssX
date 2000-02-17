91ef7d965fe9b595c8497837c9cf1917ecd3e6b6
dawes <dawes>
2000-02-17 16:53:03 +0000

    fix ident lines

--- programs/xmessage/makeform.c
+++ programs/xmessage/makeform.c
@@ -28,7 +28,7 @@ other dealings in this Software without prior written authorization
 from the X Consortium.
 
 */
-/* $XFree86 */
+/* $XFree86$ */
 
 #include <X11/Intrinsic.h>
 #include <X11/StringDefs.h>
