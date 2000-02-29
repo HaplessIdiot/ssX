92da8c59ebc27a776010229a02d22ffe204ed206
mvojkovi <mvojkovi>
2000-02-29 00:16:03 +0000

    fix some REGION_BROKEN stuff in the overlay code.

--- programs/Xserver/mi/mioverlay.h
+++ programs/Xserver/mi/mioverlay.h
@@ -23,5 +23,6 @@ Bool miOverlayCollectUnderlayRegions(WindowPtr, RegionPtr*);
 void miOverlayComputeCompositeClip(GCPtr, WindowPtr);
 Bool miOverlayCopyUnderlay(ScreenPtr);
 void miOverlaySetTransFunction(ScreenPtr, miOverlayTransFunc);
+void miOverlaySetRootClip(ScreenPtr, Bool);
 
 #endif /* __MIOVERLAY_H */
