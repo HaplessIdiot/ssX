bec3d173f2eb3d7e796b96df86a6ca98e4a6b5f0
alanh <alanh>
2000-06-21 13:51:27 +0000

    turn off debug message.

--- programs/Xserver/hw/xfree86/drivers/glint/glint_dri.c
+++ programs/Xserver/hw/xfree86/drivers/glint/glint_dri.c
@@ -1,4 +1,4 @@
-/* $XFree86: xc/programs/Xserver/hw/xfree86/drivers/glint/glint_dri.c,v 1.15 2000/06/16 01:50:21 dawes Exp $ */
+/* $XFree86: xc/programs/Xserver/hw/xfree86/drivers/glint/glint_dri.c,v 1.16 2000/06/17 10:00:13 alanh Exp $ */
 /**************************************************************************
 
 Copyright 1998-1999 Precision Insight, Inc., Cedar Park, Texas.
@@ -658,7 +658,7 @@ GLINTCreateContext(ScreenPtr pScreen,
     if (!pGlintConfig) 
 	return TRUE; /* no GLX driver private support, yet */
 
-#if 1
+#if 0
     xf86DrvMsg(pScrn->scrnIndex, X_INFO,
 	       "[GLINTCreateContext] context priv index = %d\n",
 	       pGlintConfig->index);
