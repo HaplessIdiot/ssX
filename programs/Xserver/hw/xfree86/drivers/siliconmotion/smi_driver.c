f9ba6f00d2ae1318739bc920b81dbdab97fe8022
alanh <alanh>
2000-12-01 14:31:37 +0000

    fix silicon motion driver name

--- programs/Xserver/hw/xfree86/drivers/siliconmotion/smi_driver.c
+++ programs/Xserver/hw/xfree86/drivers/siliconmotion/smi_driver.c
@@ -26,7 +26,7 @@ Silicon Motion shall not be used in advertising or otherwise to promote the
 sale, use or other dealings in this Software without prior written
 authorization from The XFree86 Project or Silicon Motion.
 */
-/* $XFree86$ */
+/* $XFree86: xc/programs/Xserver/hw/xfree86/drivers/siliconmotion/smi_driver.c,v 1.1 2000/11/28 20:59:19 dawes Exp $ */
 
 #include "xf86Resources.h"
 #include "xf86RAC.h"
@@ -84,7 +84,7 @@ static void SMI_FreeScreen(int ScrnIndex, int flags);
 static void SMI_ProbeDDC(ScrnInfoPtr pScrn, int index);
 
 
-#define SILICONMOTION_NAME			"Silicon Motion"
+#define SILICONMOTION_NAME		"SiliconMotion"
 #define SILICONMOTION_DRIVER_NAME	"siliconmotion"
 #define SILICONMOTION_VERSION_NAME	"1.1.8"
 #define SILICONMOTION_VERSION_MAJOR	1
