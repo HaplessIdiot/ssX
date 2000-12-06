e61d93f00c6a06bfd1f70d534565f4956bb36620
mvojkovi <mvojkovi>
2000-12-06 01:40:49 +0000

    Fix default contrast value on i810 Xv support.

--- programs/Xserver/hw/xfree86/drivers/i810/i810_video.c
+++ programs/Xserver/hw/xfree86/drivers/i810/i810_video.c
@@ -23,7 +23,7 @@ OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR
 THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 
 **************************************************************************/
-/* $XFree86: xc/programs/Xserver/hw/xfree86/drivers/i810/i810_video.c,v 1.12 2000/09/17 01:36:27 mvojkovi Exp $ */
+/* $XFree86: xc/programs/Xserver/hw/xfree86/drivers/i810/i810_video.c,v 1.13 2000/12/06 00:08:49 mvojkovi Exp $ */
 
 /*
  * i810_video.c: i810 Xv driver. Based on the mga Xv driver by Mark Vojkovich.
@@ -372,7 +372,7 @@ I810SetupImageVideo(ScreenPtr pScreen)
     pPriv->colorKey = pI810->colorKey & ((1 << pScrn->depth) - 1);
     pPriv->videoStatus = 0;
     pPriv->brightness = 0;
-    pPriv->contrast = 128;
+    pPriv->contrast = 64;
     pPriv->linear = NULL;
     pPriv->currentBuf = 0;
 
