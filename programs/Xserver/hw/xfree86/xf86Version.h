df8b1b519dd6671f5ebf44d3c2eea4f4aca9b4c6
dawes <dawes>
2000-12-12 17:18:58 +0000

    update date, etc

--- programs/Xserver/hw/xfree86/xf86Version.h
+++ programs/Xserver/hw/xfree86/xf86Version.h
@@ -1,4 +1,4 @@
-/* $XFree86: xc/programs/Xserver/hw/xfree86/xf86Version.h,v 3.455 2000/12/08 16:37:17 dawes Exp $ */
+/* $XFree86: xc/programs/Xserver/hw/xfree86/xf86Version.h,v 3.456 2000/12/11 19:51:05 dawes Exp $ */
 
 #define XF86_VERSION " 4.0.2 (RC2) "
 
@@ -18,6 +18,6 @@
 			XF86_VERSION_BETA,			\
 			XF86_VERSION_ALPHA)
 
-#define XF86_DATE	"11 December 2000"
+#define XF86_DATE	"12 December 2000"
 
 /* $XConsortium: xf86Version.h /main/78 1996/10/28 05:42:10 kaleb $ */
