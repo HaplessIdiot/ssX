ed665c9761d1860a2f97d3c9ef8c2edb3d2f0912
dawes <dawes>
2001-01-19 17:10:50 +0000

    We have pCompositeClip in the GC rather than in a GC private.

--- programs/Xserver/Xprint/pcl/Pcl.h
+++ programs/Xserver/Xprint/pcl/Pcl.h
@@ -44,7 +44,7 @@ not be used in advertising or otherwise to promote the sale, use or other
 dealings in this Software without prior written authorization from said
 copyright holders.
 */
-/* $XFree86: xc/programs/Xserver/Xprint/pcl/Pcl.h,v 1.5 1999/12/13 02:12:53 robin Exp $ */
+/* $XFree86: xc/programs/Xserver/Xprint/pcl/Pcl.h,v 1.6 2001/01/17 22:36:30 dawes Exp $ */
 
 #ifndef _PCL_H_
 #define _PCL_H_
@@ -554,7 +554,6 @@ extern Bool PclPositionWindow(
     register WindowPtr pWin,
     int x,
     int y);
-extern Bool PclUnmapWindow(WindowPtr pWindow);
 extern Bool PclUnmapWindow(WindowPtr pWindow);
 extern void PclCopyWindow(
     WindowPtr pWin,
