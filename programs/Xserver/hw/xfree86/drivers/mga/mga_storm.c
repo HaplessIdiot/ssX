2b3c2ba370a946dff4555a28cc5deddb7f24b064
eich <eich>
2001-02-15 18:16:18 +0000

    164. Added option to swap heads on a matrox G400 dual head (Stefan Dirsch).  163. Added handling of broken planemask support on different models of       matrox G100 cards (Egbert Eich).

--- programs/Xserver/hw/xfree86/drivers/mga/mga_storm.c
+++ programs/Xserver/hw/xfree86/drivers/mga/mga_storm.c
@@ -1,4 +1,4 @@
-/* $XFree86: xc/programs/Xserver/hw/xfree86/drivers/mga/mga_storm.c,v 1.81 2000/12/14 16:33:09 eich Exp $ */
+/* $XFree86: xc/programs/Xserver/hw/xfree86/drivers/mga/mga_storm.c,v 1.82 2001/02/15 11:03:57 alanh Exp $ */
 
 
 /* All drivers should typically include these */
@@ -1032,8 +1032,11 @@ MGAStormEngineInit(ScrnInfoPtr pScrn)
     pMga->MAccess = maccess;
     pMga->PlaneMask = ~0;
     /* looks like this doesn't apply to mga g100 pci */
-    if (pMga->Chipset != PCI_CHIP_MGAG100) 
-	OUTREG(MGAREG_PLNWT, pMga->PlaneMask);
+
+    if ((pMga->Chipset != PCI_CHIP_MGAG100) 
+	&& (pMga->Chipset != PCI_CHIP_MGAG100_PCI))
+        OUTREG(MGAREG_PLNWT, pMga->PlaneMask);
+
     pMga->FgColor = 0;
     OUTREG(MGAREG_FCOL, pMga->FgColor);
     pMga->BgColor = 0;
