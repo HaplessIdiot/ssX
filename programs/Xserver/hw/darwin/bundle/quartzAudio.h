9aba74a8344556ec2d42f070294745f851c46ca8
tsi <tsi>
2001-04-01 20:45:43 +0000

    missing newline

--- programs/Xserver/hw/darwin/bundle/quartzAudio.h
+++ programs/Xserver/hw/darwin/bundle/quartzAudio.h
@@ -3,7 +3,7 @@
 //
 // X Window bell support using CoreAudio or AppKit.
 // Greg Parker   gparker@cs.stanford.edu   19 Feb 2001
-/* $XFree86: $ */
+/* $XFree86: xc/programs/Xserver/hw/darwin/bundle/quartzAudio.h,v 1.1 2001/04/01 07:12:15 torrey Exp $ */
 
 #ifndef _QUARTZAUDIO_H
 #define _QUARTZAUDIO_H
