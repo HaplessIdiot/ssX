f9936c13f9b36f8dbb9970fb4e3fe2f7feb1a249
dawes <dawes>
2001-04-08 16:33:22 +0000

    xvmc.h is replaced by xvmcext.h

--- programs/Xserver/Xext/xvmodproc.h
+++ programs/Xserver/Xext/xvmodproc.h
@@ -1,6 +1,6 @@
-/* $XFree86: xc/programs/Xserver/Xext/xvmodproc.h,v 1.1 1998/08/13 14:45:36 dawes Exp $ */
+/* $XFree86: xc/programs/Xserver/Xext/xvmodproc.h,v 1.2 2001/03/05 04:51:55 mvojkovi Exp $ */
 
-#include "xvmc.h"
+#include "xvmcext.h"
 
 extern int (*XvGetScreenIndexProc)(void);
 extern unsigned long (*XvGetRTPortProc)(void);
