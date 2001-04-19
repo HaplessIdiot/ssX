11a54f0771d91e191cf384f9db66fb1934087a14
alanh <alanh>
2001-04-19 12:45:22 +0000

    new files

---
+++ programs/Xserver/hw/xfree86/drivers/sis/sis_shadow.h
@@ -0,0 +1,6 @@
+void SISPointerMoved(int index, int x, int y);
+void SISRefreshArea(ScrnInfoPtr pScrn, int num, BoxPtr pbox);
+void SISRefreshArea8(ScrnInfoPtr pScrn, int num, BoxPtr pbox);
+void SISRefreshArea16(ScrnInfoPtr pScrn, int num, BoxPtr pbox);
+void SISRefreshArea24(ScrnInfoPtr pScrn, int num, BoxPtr pbox);
+void SISRefreshArea32(ScrnInfoPtr pScrn, int num, BoxPtr pbox);
