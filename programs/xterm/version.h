2499d82f568f44f06403583160cf7ee5658fedc2
dickey <dickey>
2001-04-20 09:30:38 +0000

    + correct return type of in_put() from patch #153 changes, which 	  left it not wide enough for UTF-8 (patch by Bruno Haible).

--- programs/xterm/version.h
+++ programs/xterm/version.h
@@ -1,4 +1,4 @@
-/* $XFree86: xc/programs/xterm/version.h,v 3.65 2001/03/29 22:58:55 dickey Exp $ */
+/* $XFree86: xc/programs/xterm/version.h,v 3.66 2001/04/12 01:02:51 dickey Exp $ */
 
 /*
  * These definitions are used to build the string that's printed in response to
@@ -6,5 +6,5 @@
  * XFree86 to which this version of xterm has been built.  The number in
  * parentheses is my patch number (T.Dickey).
  */
-#define XTERM_PATCH   154
+#define XTERM_PATCH   155
 #define XFREE86_VERSION "XFree86 4.0.3"
