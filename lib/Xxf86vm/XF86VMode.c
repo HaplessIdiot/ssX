112388cb3a8823e71d956825ad7f04a2d94f77d4
mvojkovi <mvojkovi>
2001-05-07 20:11:13 +0000

    Fix typo.

--- lib/Xxf86vm/XF86VMode.c
+++ lib/Xxf86vm/XF86VMode.c
@@ -1,5 +1,5 @@
 /* $XConsortium: XF86VMode.c /main/2 1995/11/14 18:17:58 kaleb $ */
-/* $XFree86: xc/lib/Xxf86vm/XF86VMode.c,v 3.29 2000/08/22 16:14:47 alanh Exp $ */
+/* $XFree86: xc/lib/Xxf86vm/XF86VMode.c,v 3.30 2001/05/06 00:51:56 mvojkovi Exp $ */
 /*
 
 Copyright (c) 1995  Kaleb S. KEITHLEY
@@ -1178,7 +1178,7 @@ XF86VidModeGetGammaRamp (
     return True;
 }
 
-Bool XF86VidModeSetGammaRampSize(
+Bool XF86VidModeGetGammaRampSize(
     Display *dpy,
     int screen,
     int *size
