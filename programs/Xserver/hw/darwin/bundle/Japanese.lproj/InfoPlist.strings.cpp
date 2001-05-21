0a380032e9aca69fa8ad038a4dea0da13e028d04
torrey <torrey>
2001-05-21 01:42:14 +0000

    Update XDarwin man pages, help files, and version info. Add option to build XDarwin.app bundle for deployment.

---
+++ programs/Xserver/hw/darwin/bundle/Japanese.lproj/InfoPlist.strings.cpp
@@ -0,0 +1,5 @@
+/* Japanese localized versions of Info.plist keys. */
+/* Most of these are set in the target application settings. */
+/* $XFree86: $ */
+
+NSHumanReadableCopyright = XF86_COPYRIGHT
