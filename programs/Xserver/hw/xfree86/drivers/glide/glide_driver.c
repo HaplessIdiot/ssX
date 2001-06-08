c10c35ef496860569aef1141ec85c54abdd263dd
alanh <alanh>
2001-06-08 10:53:36 +0000

    fix broken glide driver that caused -configure to fail for all drivers in 4.1.0

--- programs/Xserver/hw/xfree86/drivers/glide/glide_driver.c
+++ programs/Xserver/hw/xfree86/drivers/glide/glide_driver.c
@@ -45,7 +45,7 @@
    * Support static loading.  
 */
 
-/* $XFree86: xc/programs/Xserver/hw/xfree86/drivers/glide/glide_driver.c,v 1.23 2001/05/04 19:05:37 dawes Exp $ */
+/* $XFree86: xc/programs/Xserver/hw/xfree86/drivers/glide/glide_driver.c,v 1.24 2001/05/16 06:48:08 keithp Exp $ */
 
 #include "xaa.h"
 #include "xf86Cursor.h"
@@ -250,14 +250,6 @@ glideSetup(pointer module, pointer opts, int *errmaj, int *errmin)
   static Bool setupDone = FALSE;
   pointer ret;
   int errmaj2 = 0, errmin2 = 0;
-
-  /* This module should be loaded only once, but check to be sure. */
-
-  if (xf86ServerIsOnlyDetecting())
-  {
-    xf86AddDriver(&GLIDE, module, 0);
-    return (pointer)1;
-  }
     
   if (!setupDone) 
   {
@@ -299,6 +291,8 @@ glideSetup(pointer module, pointer opts, int *errmaj, int *errmin)
     }
 
     setupDone = TRUE;
+    /* This module should be loaded only once */
+    *errmaj = LDR_ONCEONLY;
     xf86AddDriver(&GLIDE, module, 0);
 
     /*
