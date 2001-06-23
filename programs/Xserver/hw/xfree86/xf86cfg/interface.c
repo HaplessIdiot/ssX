a27943072783c3811d026d04d652b98298a7d9d9
paulo <paulo>
2001-06-23 01:45:56 +0000

    Made the "Add standard VESA mode to current screen" menu useful. XF86VidModeAddModeLine apparently always returns true. With this patch, if no new modelines were added, it will ask the user to add the modeline definition to the Monitor section.

--- programs/Xserver/hw/xfree86/xf86cfg/interface.c
+++ programs/Xserver/hw/xfree86/xf86cfg/interface.c
@@ -26,7 +26,7 @@
  *
  * Author: Paulo César Pereira de Andrade <pcpa@conectiva.com.br>
  *
- * $XFree86: xc/programs/Xserver/hw/xfree86/xf86cfg/interface.c,v 1.24 2001/05/21 22:21:57 paulo Exp $
+ * $XFree86: xc/programs/Xserver/hw/xfree86/xf86cfg/interface.c,v 1.25 2001/06/22 23:22:20 paulo Exp $
  */
 
 #include <X11/IntrinsicP.h>
@@ -168,7 +168,7 @@ static XtActionsRec actions[] = {
     {"options-cancel", OptionsCancelAction},
     {"error-cancel", ErrorCancelAction},
     {"quit-cancel", QuitCancelAction},
-    {"addmode-cancel", CancelForceAddModeAction},
+    {"addmode-cancel", CancelAddModeAction},
     {"accessx-close", CloseAccessXAction},
     {"testmode-cancel", CancelTestModeAction},
     {"help-close", HelpCancelAction},
