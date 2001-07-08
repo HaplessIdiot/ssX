1fa2c7101ab5bbdacbf348670a0a763cb1c15d29
herrb <herrb>
2001-07-08 22:20:53 +0000

    bump nv driver patchlevel

--- programs/Xserver/hw/xfree86/drivers/nv/nv_const.h
+++ programs/Xserver/hw/xfree86/drivers/nv/nv_const.h
@@ -1,4 +1,4 @@
-/* $XFree86: xc/programs/Xserver/hw/xfree86/drivers/nv/nv_const.h,v 1.3 2000/02/25 21:02:55 dawes Exp $ */
+/* $XFree86: xc/programs/Xserver/hw/xfree86/drivers/nv/nv_const.h,v 1.4 2000/10/24 22:45:08 dawes Exp $ */
 
 #ifndef __NV_CONST_H__
 #define __NV_CONST_H__
@@ -8,7 +8,7 @@
 #define NV_DRIVER_NAME "nv"
 #define NV_MAJOR_VERSION 1
 #define NV_MINOR_VERSION 0
-#define NV_PATCHLEVEL 0
+#define NV_PATCHLEVEL 1
 
 #define NV_USE_FB
 
