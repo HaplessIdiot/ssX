a206583b1b283883919fe413d8db9c738847eda0
tsi <tsi>
2001-08-23 21:49:51 +0000

    Ident lines

--- programs/Xserver/xkb/xkbLEDs.c
+++ programs/Xserver/xkb/xkbLEDs.c
@@ -24,6 +24,7 @@ OR OTHER TORTIOUS ACTION, ARISING OUT OF OR IN CONNECTION  WITH
 THE USE OR PERFORMANCE OF THIS SOFTWARE.
 
 ********************************************************/
+/* $XFree86$ */
 
 #include <stdio.h>
 #include <ctype.h>
