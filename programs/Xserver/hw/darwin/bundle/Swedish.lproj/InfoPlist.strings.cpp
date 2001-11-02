e1599da5350b37256fc95e4ce585293f997bc4af
torrey <torrey>
2001-11-02 02:47:23 +0000

    Add Swedish localization of XDarwin front end (Patrik Montgomery).

---
+++ programs/Xserver/hw/darwin/bundle/Swedish.lproj/InfoPlist.strings.cpp
@@ -0,0 +1,5 @@
+/* Swedish localized versions of Info.plist keys. */
+/* Most of these are set in the target application settings. */
+/* $XFree86: xc/programs/Xserver/hw/darwin/bundle/English.lproj/InfoPlist.strings.cpp,v 1.1 2001/05/21 01:42:17 torrey Exp $ */
+
+NSHumanReadableCopyright = XF86_COPYRIGHT
