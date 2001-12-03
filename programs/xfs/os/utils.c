96fb678a8c541a4f0743af590c02fb1ea3d21f7e
herrb <herrb>
2001-12-03 21:25:46 +0000

    signal handler fixes: avoid stdio & preserve errno

--- programs/xfs/os/utils.c
+++ programs/xfs/os/utils.c
@@ -42,7 +42,7 @@ in this Software without prior written authorization from The Open Group.
  * ACTION, ARISING OUT OF OR IN CONNECTION WITH THE USE OR PERFORMANCE OF
  * THIS SOFTWARE.
  */
-/* $XFree86: xc/programs/xfs/os/utils.c,v 3.14 2001/06/26 22:18:14 paulo Exp $ */
+/* $XFree86: xc/programs/xfs/os/utils.c,v 3.15 2001/07/25 15:05:22 dawes Exp $ */
 
 #include	<stdio.h>
 #include	<X11/Xos.h>
@@ -57,6 +57,7 @@ in this Software without prior written authorization from The Open Group.
 #include	<unistd.h>
 #include	<pwd.h>
 #include	<grp.h>
+#include	<errno.h>
 #include	<sys/types.h>
 
 #ifndef X_NOT_POSIX
@@ -100,13 +101,20 @@ Bool        portFromCmdline = FALSE;
 OldListenRec *OldListen = NULL;
 int 	     OldListenCount = 0;
 
+#ifdef STDERR_FILENO
+# define WRITES write(STDERR_FILENO, s, strlen(s))
+#else
+# define WRITES write(fileno(stderr), s, strlen(s))
+#endif
+
 /* ARGSUSED */
 SIGVAL
 AutoResetServer(int n)
 {
+    int olderrno = errno;
 
 #ifdef DEBUG
-    fprintf(stderr, "got a reset signal\n");
+    WRITES("got a reset signal\n");
 #endif
 
     dispatchException |= DE_RESET;
@@ -115,28 +123,31 @@ AutoResetServer(int n)
 #ifdef SIGNALS_RESET_WHEN_CAUGHT
     signal(SIGHUP, AutoResetServer);
 #endif
+    errno = olderrno;
 }
 
 /* ARGSUSED */
 SIGVAL
 GiveUp(int n)
 {
-
+    int olderrno = errno;
 #ifdef DEBUG
-    fprintf(stderr, "got a TERM signal\n");
+    WRITES("got a TERM signal\n");
 #endif
 
     dispatchException |= DE_TERMINATE;
     isItTimeToYield = TRUE;
+    errno = olderrno;
 }
 
 /* ARGSUSED */
 SIGVAL
 ServerReconfig(int n)
 {
+    int olderrno = errno;
 
 #ifdef DEBUG
-    fprintf(stderr, "got a re-config signal\n");
+    WRITES("got a re-config signal\n");
 #endif
 
     dispatchException |= DE_RECONFIG;
@@ -145,15 +156,17 @@ ServerReconfig(int n)
 #ifdef SIGNALS_RESET_WHEN_CAUGHT
     signal(SIGUSR1, ServerReconfig);
 #endif
+    errno = olderrno;
 }
 
 /* ARGSUSED */
 SIGVAL
 ServerCacheFlush(int n)
 {
+    int olderrno = errno;
 
 #ifdef DEBUG
-    fprintf(stderr, "got a flush signal\n");
+    WRITES("got a flush signal\n");
 #endif
 
     dispatchException |= DE_FLUSH;
@@ -162,15 +175,17 @@ ServerCacheFlush(int n)
 #ifdef SIGNALS_RESET_WHEN_CAUGHT
     signal(SIGUSR2, ServerCacheFlush);
 #endif
+    errno = olderrno;
 }
 
 /* ARGSUSED */
 SIGVAL
 CleanupChild(int n)
 {
+    int olderrno = errno;
 
 #ifdef DEBUG
-    fprintf(stderr, "got a child signal\n");
+    WRITES("got a child signal\n");
 #endif
 
     wait(NULL);
@@ -178,6 +193,7 @@ CleanupChild(int n)
 #ifdef SIGNALS_RESET_WHEN_CAUGHT
     signal(SIGCHLD, CleanupChild);
 #endif
+    errno = olderrno;
 }
 
 long
