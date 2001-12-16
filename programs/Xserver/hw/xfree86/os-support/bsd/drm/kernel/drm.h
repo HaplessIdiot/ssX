abe9783b0125f7149602adeb62abf354065be0ed
alanh <alanh>
2001-12-16 19:45:01 +0000

    enable i830 for *bsd, but kernel module has not been ported.

--- programs/Xserver/hw/xfree86/os-support/bsd/drm/kernel/drm.h
+++ programs/Xserver/hw/xfree86/os-support/bsd/drm/kernel/drm.h
@@ -93,9 +93,10 @@ typedef struct drm_tex_region {
 	unsigned int	age;
 } drm_tex_region_t;
 
-/* Seperate include files for the i810/mga/r128 specific structures */
+/* Seperate include files for the driver specific structures */
 #include "mga_drm.h"
 #include "i810_drm.h"
+#include "i830_drm.h"
 #include "r128_drm.h"
 #include "radeon_drm.h"
 #include "sis_drm.h"
@@ -485,5 +486,16 @@ typedef struct drm_scatter_gather {
 #define SIS_IOCTL_FLIP			DRM_IOW( 0x48, drm_sis_flip_t)
 #define SIS_IOCTL_FLIP_INIT		DRM_IO(  0x49)
 #define SIS_IOCTL_FLIP_FINAL		DRM_IO(  0x50)
+
+/* I830 specific ioctls */
+#define DRM_IOCTL_I830_INIT		DRM_IOW( 0x40, drm_i830_init_t)
+#define DRM_IOCTL_I830_VERTEX		DRM_IOW( 0x41, drm_i830_vertex_t)
+#define DRM_IOCTL_I830_CLEAR		DRM_IOW( 0x42, drm_i830_clear_t)
+#define DRM_IOCTL_I830_FLUSH		DRM_IO ( 0x43)
+#define DRM_IOCTL_I830_GETAGE		DRM_IO ( 0x44)
+#define DRM_IOCTL_I830_GETBUF		DRM_IOWR(0x45, drm_i830_dma_t)
+#define DRM_IOCTL_I830_SWAP		DRM_IO ( 0x46)
+#define DRM_IOCTL_I830_COPY		DRM_IOW( 0x47, drm_i830_copy_t)
+#define DRM_IOCTL_I830_DOCOPY		DRM_IO ( 0x48)
 
 #endif
