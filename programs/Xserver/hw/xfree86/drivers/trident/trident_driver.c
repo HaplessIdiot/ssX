8530f63bf4e49be1711d771e03453c5bfa3fe4cd
alanh <alanh>
2001-12-19 21:12:47 +0000

    587. Fix Trident driver pitch values when accelerator is in use, applies       to Image and Blade series chips (Alan Hourihane).

--- programs/Xserver/hw/xfree86/drivers/trident/trident_driver.c
+++ programs/Xserver/hw/xfree86/drivers/trident/trident_driver.c
@@ -28,7 +28,7 @@
  *	    Massimiliano Ghilardi, max@Linuz.sns.it, some fixes to the
  *				   clockchip programming code.
  */
-/* $XFree86: xc/programs/Xserver/hw/xfree86/drivers/trident/trident_driver.c,v 1.153 2001/12/11 22:23:03 alanh Exp $ */
+/* $XFree86: xc/programs/Xserver/hw/xfree86/drivers/trident/trident_driver.c,v 1.154 2001/12/13 18:01:51 eich Exp $ */
 
 #include "xf1bpp.h"
 #include "xf4bpp.h"
@@ -2074,7 +2074,7 @@ TRIDENTPreInit(ScrnInfoPtr pScrn, int flags)
     }
 
     /* Select valid modes from those available */
-    if (pTrident->NoAccel) {
+    if (pTrident->NoAccel || Is3Dchip) {
 	/*
 	 * XXX Assuming min pitch 256, max 4096
 	 * XXX Assuming min height 128, max 4096
