79724c126dfad6d3146d968a897f946a56eedfb8
tsi <tsi>
2001-12-20 19:28:54 +0000

    Ident line

--- include/extensions/lbximage.h
+++ include/extensions/lbximage.h
@@ -25,6 +25,7 @@ used in advertising or otherwise to promote the sale, use or other dealings
 in this Software without prior written authorization from The Open Group.
 
 ******************************************************************************/
+/* $XFree86$ */
 
 #ifndef _LBX_IMAGE_H_
 #define _LBX_IMAGE_H_
