ccb521a9704a339fb4a02a8db5c5b342f727919d
keithp <keithp>
2002-02-15 06:02:05 +0000

    libxml2 build fixes to match regular library build

--- lib/libxml2/config.h
+++ lib/libxml2/config.h
@@ -1,43 +1,44 @@
+/* config.h.  Generated automatically by configure.  */
 /* config.h.in.  Generated automatically from configure.in by autoheader.  */
 
 /* Define if you have the strftime function.  */
-#undef HAVE_STRFTIME
+#define HAVE_STRFTIME 1
 
 /* Define if you have the ANSI C header files.  */
 #define STDC_HEADERS 1
 
-#undef PACKAGE
-#undef VERSION
+#define PACKAGE "libxml2"
+#define VERSION "2.4.14"
 #define HAVE_LIBZ 1
-#define HAVE_LIBM 1
+/* #undef HAVE_LIBM */
 #define HAVE_ISINF 1
-#define HAVE_ISNAN 1
-#undef HAVE_LIBHISTORY
-#undef HAVE_LIBREADLINE
+/* #undef HAVE_ISNAN */
+/* #undef HAVE_LIBHISTORY */
+/* #undef HAVE_LIBREADLINE */
 #define SOCKLEN_T socklen_t
-#undef HAVE_LIBPTHREAD
-#undef HAVE_PTHREAD_H
+/* #undef HAVE_LIBPTHREAD */
+/* #undef HAVE_PTHREAD_H */
 
 /* Define if you have the _stat function.  */
-#undef HAVE__STAT
+/* #undef HAVE__STAT */
 
 /* Define if you have the class function.  */
-#undef HAVE_CLASS
+/* #undef HAVE_CLASS */
 
 /* Define if you have the finite function.  */
 #define HAVE_FINITE 1
 
 /* Define if you have the fp_class function.  */
-#undef HAVE_FP_CLASS
+/* #undef HAVE_FP_CLASS */
 
 /* Define if you have the fpclass function.  */
-#undef HAVE_FPCLASS
+/* #undef HAVE_FPCLASS */
 
 /* Define if you have the fprintf function.  */
 #define HAVE_FPRINTF 1
 
 /* Define if you have the isnand function.  */
-#define HAVE_ISNAND 1
+/* #undef HAVE_ISNAND */
 
 /* Define if you have the localtime function.  */
 #define HAVE_LOCALTIME 1
@@ -61,28 +62,28 @@
 #define HAVE_STAT 1
 
 /* Define if you have the strdup function.  */
-#undef HAVE_STRDUP
+#define HAVE_STRDUP 1
 
 /* Define if you have the strerror function.  */
-#undef HAVE_STRERROR
+#define HAVE_STRERROR 1
 
 /* Define if you have the strftime function.  */
-#undef HAVE_STRFTIME
+#define HAVE_STRFTIME 1
 
 /* Define if you have the strndup function.  */
-#undef HAVE_STRNDUP
+#define HAVE_STRNDUP 1
 
 /* Define if you have the vfprintf function.  */
-#undef HAVE_VFPRINTF
+#define HAVE_VFPRINTF 1
 
 /* Define if you have the vsnprintf function.  */
-#undef HAVE_VSNPRINTF
+#define HAVE_VSNPRINTF 1
 
 /* Define if you have the vsprintf function.  */
-#undef HAVE_VSPRINTF
+#define HAVE_VSPRINTF 1
 
 /* Define if you have the <ansidecl.h> header file.  */
-#undef HAVE_ANSIDECL_H
+/* #undef HAVE_ANSIDECL_H */
 
 /* Define if you have the <arpa/inet.h> header file.  */
 #define HAVE_ARPA_INET_H 1
@@ -94,7 +95,7 @@
 #define HAVE_DIRENT_H 1
 
 /* Define if you have the <dlfcn.h> header file.  */
-#undef HAVE_DLFCN_H
+#define HAVE_DLFCN_H 1
 
 /* Define if you have the <errno.h> header file.  */
 #define HAVE_ERRNO_H 1
@@ -103,25 +104,25 @@
 #define HAVE_FCNTL_H 1
 
 /* Define if you have the <float.h> header file.  */
-#undef HAVE_FLOAT_H
+#define HAVE_FLOAT_H 1
 
 /* Define if you have the <fp_class.h> header file.  */
-#undef HAVE_FP_CLASS_H
+/* #undef HAVE_FP_CLASS_H */
 
 /* Define if you have the <ieeefp.h> header file.  */
-#undef HAVE_IEEEFP_H
+/* #undef HAVE_IEEEFP_H */
 
 /* Define if you have the <malloc.h> header file.  */
-#undef HAVE_MALLOC_H
+#define HAVE_MALLOC_H 1
 
 /* Define if you have the <math.h> header file.  */
 #define HAVE_MATH_H 1
 
 /* Define if you have the <nan.h> header file.  */
-#undef HAVE_NAN_H
+/* #undef HAVE_NAN_H */
 
 /* Define if you have the <ndir.h> header file.  */
-#undef HAVE_NDIR_H
+/* #undef HAVE_NDIR_H */
 
 /* Define if you have the <netdb.h> header file.  */
 #define HAVE_NETDB_H 1
@@ -130,65 +131,65 @@
 #define HAVE_NETINET_IN_H 1
 
 /* Define if you have the <signal.h> header file.  */
-#define HAVE_SIGNAL_H
+#define HAVE_SIGNAL_H 1
 
 /* Define if you have the <stdarg.h> header file.  */
-#define HAVE_STDARG_H
+#define HAVE_STDARG_H 1
 
 /* Define if you have the <stdlib.h> header file.  */
-#define HAVE_STDLIB_H
+#define HAVE_STDLIB_H 1
 
 /* Define if you have the <string.h> header file.  */
-#undef HAVE_STRING_H
+/* #undef HAVE_STRING_H */
 
 /* Define if you have the <sys/dir.h> header file.  */
-#undef HAVE_SYS_DIR_H
+/* #undef HAVE_SYS_DIR_H */
 
 /* Define if you have the <sys/mman.h> header file.  */
-#undef HAVE_SYS_MMAN_H
+#define HAVE_SYS_MMAN_H 1
 
 /* Define if you have the <sys/ndir.h> header file.  */
-#undef HAVE_SYS_NDIR_H
+/* #undef HAVE_SYS_NDIR_H */
 
 /* Define if you have the <sys/select.h> header file.  */
-#undef HAVE_SYS_SELECT_H
+#define HAVE_SYS_SELECT_H 1
 
 /* Define if you have the <sys/socket.h> header file.  */
-#define HAVE_SYS_SOCKET_H
+#define HAVE_SYS_SOCKET_H 1
 
 /* Define if you have the <sys/stat.h> header file.  */
-#define HAVE_SYS_STAT_H
+#define HAVE_SYS_STAT_H 1
 
 /* Define if you have the <sys/time.h> header file.  */
-#define HAVE_SYS_TIME_H
+#define HAVE_SYS_TIME_H 1
 
 /* Define if you have the <sys/types.h> header file.  */
-#define HAVE_SYS_TYPES_H
+#define HAVE_SYS_TYPES_H 1
 
 /* Define if you have the <time.h> header file.  */
-#define HAVE_TIME_H
+#define HAVE_TIME_H 1
 
 /* Define if you have the <unistd.h> header file.  */
-#define HAVE_UNISTD_H
+#define HAVE_UNISTD_H 1
 
 /* Define if you have the <zlib.h> header file.  */
-#define HAVE_ZLIB_H
+#define HAVE_ZLIB_H 1
 
 /* Define if you have the inet library (-linet).  */
-#undef HAVE_LIBINET
+/* #undef HAVE_LIBINET */
 
 /* Define if you have the nsl library (-lnsl).  */
-#undef HAVE_LIBNSL
+/* #undef HAVE_LIBNSL */
 
 /* Define if you have the socket library (-lsocket).  */
-#undef HAVE_LIBSOCKET
+/* #undef HAVE_LIBSOCKET */
 
 /* Name of package */
-#undef PACKAGE
+#define PACKAGE "libxml2"
 
 /* Version number of package */
-#undef VERSION
+#define VERSION "2.4.14"
 
 /* Define if compiler has function prototypes */
-#define PROTOTYPES
+#define PROTOTYPES 1
 
