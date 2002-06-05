fa468f057b19c77b515a9434cac33b6104a281fe
dawes <dawes>
2002-06-05 20:27:34 +0000

    set date

--- programs/Xserver/hw/xfree86/xf86Version.h
+++ programs/Xserver/hw/xfree86/xf86Version.h
@@ -1,4 +1,4 @@
-/* $XFree86: xc/programs/Xserver/hw/xfree86/xf86Version.h,v 3.489 2002/05/31 18:45:56 dawes Exp $ */
+/* $XFree86: xc/programs/Xserver/hw/xfree86/xf86Version.h,v 3.490 2002/06/03 18:29:08 dawes Exp $ */
 
 #ifndef XF86_VERSION_CURRENT
 
@@ -24,7 +24,7 @@
 			0)
 
 
-#define XF86_DATE	"xx June 2002"
+#define XF86_DATE	"5 June 2002"
 
 #endif
 
