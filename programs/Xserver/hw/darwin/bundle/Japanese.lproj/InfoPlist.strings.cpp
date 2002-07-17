f4e40a51db04186845fde301b16d62f2951346ab
torrey <torrey>
2002-07-17 01:24:49 +0000

    Fix Finder warnings about XDarwin plist format on Jaguar.

--- programs/Xserver/hw/darwin/bundle/Japanese.lproj/InfoPlist.strings.cpp
+++
@@ -1,5 +0,0 @@
-/* Japanese localized versions of Info.plist keys. */
-/* Most of these are set in the target application settings. */
-/* $XFree86: xc/programs/Xserver/hw/darwin/bundle/Japanese.lproj/InfoPlist.strings.cpp,v 1.1 2001/05/21 01:42:17 torrey Exp $ */
-
-NSHumanReadableCopyright = __quote__XFree86 XF86_VERSION\nCopyright 2001 XFree86 Project, Inc.__quote__
--- programs/Xserver/hw/darwin/bundle/ko.lproj/InfoPlist.strings.cpp
+++
@@ -1,5 +0,0 @@
-/* Korean localized versions of Info.plist keys. */
-/* Most of these are set in the target application settings. */
-/* $XFree86: xc/programs/Xserver/hw/darwin/bundle/English.lproj/InfoPlist.strings.cpp,v 1.2 2001/11/04 07:09:47 torrey Exp $ */
-
-NSHumanReadableCopyright = __quote__XFree86 XF86_VERSION\nCopyright 2001 XFree86 Project, Inc.__quote__
