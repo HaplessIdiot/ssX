1bdecf804846b543259c75a38e3262896d8aa22c
tsi <tsi>
2002-09-16 15:54:42 +0000

    Add a dummy xf86FreeResListi() entry.

--- programs/Xserver/hw/xfree86/dummylib/xf86addrestolist.c
+++ programs/Xserver/hw/xfree86/dummylib/xf86addrestolist.c
@@ -1,4 +1,4 @@
-/* $XFree86$ */
+/* $XFree86: xc/programs/Xserver/hw/xfree86/dummylib/xf86addrestolist.c,v 1.1tsi Exp $ */
 
 #include "xf86.h"
 
@@ -6,4 +6,10 @@ resPtr
 xf86AddResToList(resPtr rlist, resRange *Range, int entityIndex)
 {
     return rlist;
+}
+
+void
+xf86FreeResList(resPtr rlist)
+{
+    return;
 }
