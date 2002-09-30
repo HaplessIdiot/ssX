14b0e27ba682fa62a999c39e7f69da9a1b724e76
dickey <dickey>
2002-09-30 00:39:05 +0000

    xterm patch #168:
    	+ improve data reported for control sequence that requests window
    	  position by taking into account the window decorations, e.g., border
    	  and title (patch by Jess Thrysoee <jess@thrysoee.dk>).
    
    	+ add "-cjk_width" and corresponding resource "cjkWidth" (patch by
    	  Jungshik Shin <jshin@mailaps.org>).
    
    	+ add "-into" option, for embedding xterm in a Tcl/Tk application
    	  (patch by George Peter Staplin <georgeps@xmission.com>).
    
    	+ add simple session management (XSM) client capabilities to xterm.  So
    	  a session manager such as, e.g., xsm, should be able to respawn or
    	  kill xterms without the help of an SM proxy (like smproxy), which is
    	  a hack and almost always buggy (patch by David Madore).
    
    	+ fix conflict between ifdef's for "OPT_DEC_LOCATOR" and "OPT_READLINE"
    	  in button.c (reported by Ilya Zakharevich).
    
    	+ fix for inconsistent use of "struct utmp" versus "struct utmpx"
    	  introduced in patch #167 (patches by Paul Gilmartin, Marc La France).
    
    	+ modify logic for "metaSendsEscape" to allow it to work in a setup
    	  where the Meta-key is not recognized as a modifier by the
    	  key-translations logic (from a discussion of eightBitInput with
    	  H.J.Lu, however Vincent Lefevre reported this in January).
    
    	+ use null pointer values consistently, rather than literal "0", for
    	  ending variable-length argument lists, e.g., for "execlp()" (based on
    	  patch by Matthieu Herrb).
    
    	+ correct logic of "ChangeAnsiColorRequest()", which would do a screen
    	  repaint after replying to a request for information.
    
    	+ improve scripts in vttests to work with systems whose shells support
    	  "echo -n" and have, as does Debian, an unrelated "print" utility.
    
    	+ add vttests/acolors.sh to demonstrate OSC 4, which queries or sets
    	  ANSI colors.
    
    	+ fixes for ctlseqs.ms (Pavel Roskin, Ilya Zakharevich).
    
    	+ typos in xterm manpage (Jens Schweikhardt)
    
    	+ remove a redundant GCC_UNUSED from InitPopup() (Nam SungHyun)

--- programs/xterm/vttests/fonts.sh
+++ programs/xterm/vttests/fonts.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-# $XFree86: xc/programs/xterm/vttests/fonts.sh,v 1.1 1999/04/11 13:11:43 dawes Exp $
+# $XFree86: xc/programs/xterm/vttests/fonts.sh,v 1.2 2000/10/27 18:31:17 dawes Exp $
 #
 # -- Thomas Dickey (1999/3/27)
 # Demonstrate the use of dynamic colors by setting the background successively
@@ -10,6 +10,8 @@ CMD='echo'
 OPT='-n'
 SUF=''
 TMP=/tmp/xterm$$
+eval '$CMD $OPT >$TMP || echo fail >$TMP' 2>/dev/null
+( test ! -f $TMP || test -s $TMP ) &&
 for verb in printf print ; do
     rm -f $TMP
     eval '$verb "\c" >$TMP || echo fail >$TMP' 2>/dev/null
