b08e42319aeb6f4fd6dd85341864ef29b1479449
alanh <alanh>
2002-10-01 14:31:36 +0000

    358. Enable version controlled .dll's on Cygwin/XFree86 (Alexander Gottwald).  357. Some build fixes for Cygwin/XFree86 (Alan Hourihane).

--- lib/Xrender/Xrender-def.cpp
+++ lib/Xrender/Xrender-def.cpp
@@ -1,29 +1,42 @@
 LIBRARY Xrender
 VERSION LIBRARY_VERSION
 EXPORTS
-	XRenderComposite
-	XRenderFillRectangle
-	XRenderFillRectangles
-	XRenderAddGlyphs
-	XRenderCompositeString16
-	XRenderCompositeString32
-	XRenderCompositeString8
-	XRenderCreateGlyphSet
-	XRenderFreeGlyphSet
-	XRenderFreeGlyphs
-	XRenderReferenceGlyphSet
-	XRenderChangePicture
-	XRenderCreatePicture
-	XRenderFreePicture
-;	_XRenderProcessPictureAttributes
-	XRenderFindDisplay
-	XRenderFindFormat
-	XRenderFindVisualFormat
-	XRenderQueryExtension
-	XRenderQueryFormats
-	XRenderQueryVersion
-;	_XRenderFindFormat
-;	_XRenderFindVisual
-	XRenderSetPictureClipRegion	
-
-/* $XFree86: xc/lib/Xrender/Xrender-def.cpp,v 1.2 2000/12/04 21:01:00 dawes Exp $ */
+XRenderParseColor
+XRenderComposite
+XRenderCreateCursor
+XRenderFillRectangle
+XRenderFillRectangles
+XRenderQueryFilters
+XRenderSetPictureFilter
+XRenderAddGlyphs
+XRenderCompositeString16
+XRenderCompositeString32
+XRenderCompositeString8
+XRenderCompositeText16
+XRenderCompositeText32
+XRenderCompositeText8
+XRenderCreateGlyphSet
+XRenderFreeGlyphSet
+XRenderFreeGlyphs
+XRenderReferenceGlyphSet
+XRenderChangePicture
+XRenderCreatePicture
+XRenderFreePicture
+XRenderSetPictureClipRectangles
+XRenderSetPictureClipRegion
+XRenderSetPictureTransform
+XRenderCompositeDoublePoly
+XRenderCompositeTrapezoids
+XRenderCompositeTriFan
+XRenderCompositeTriStrip
+XRenderCompositeTriangles
+XRenderFindDisplay
+XRenderFindFormat
+XRenderFindStandardFormat
+XRenderFindVisualFormat
+XRenderQueryExtension
+XRenderQueryFormats
+XRenderQuerySubpixelOrder
+XRenderQueryVersion
+XRenderSetSubpixelOrder
+/* $XFree86$ */
