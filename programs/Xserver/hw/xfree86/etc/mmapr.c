301333377a80258130f16ebe0bb7309d40cbf51f
tsi <tsi>
2002-10-04 20:13:50 +0000

    Fix copyright

--- programs/Xserver/hw/xfree86/etc/mmapr.c
+++ programs/Xserver/hw/xfree86/etc/mmapr.c
@@ -1,6 +1,6 @@
-/* $XFree86: xc/programs/Xserver/hw/xfree86/etc/mmapr.c,v 1.2tsi Exp $ */
+/* $XFree86: xc/programs/Xserver/hw/xfree86/etc/mmapr.c,v 1.3 2002/10/04 20:09:00 tsi Exp $ */
 /*
- * Copyright 1997 through 2002 by Marc Aurele La France (TSI @ UQV), tsi@xfree86.org
+ * Copyright 2002 by Marc Aurele La France (TSI @ UQV), tsi@xfree86.org
  *
  * Permission to use, copy, modify, distribute, and sell this software and its
  * documentation for any purpose is hereby granted without fee, provided that
