d2a78a58e728878fd649de46f4e1dff302539172
herrb <herrb>
2002-11-09 17:26:17 +0000

    All r128 known cards only have 0x4000 bytes of MMIO space. From NetBSD. Ok dawes@.

--- programs/Xserver/hw/xfree86/drivers/ati/r128.h
+++ programs/Xserver/hw/xfree86/drivers/ati/r128.h
@@ -1,4 +1,4 @@
-/* $XFree86: xc/programs/Xserver/hw/xfree86/drivers/ati/r128.h,v 1.21 2002/10/30 12:52:11 alanh Exp $ */
+/* $XFree86: r128.h,v 1.22 2002/10/31 18:06:59 anderson Exp $ */
 /*
  * Copyright 1999, 2000 ATI Technologies Inc., Markham, Ontario,
  *                      Precision Insight, Inc., Cedar Park, Texas, and
@@ -64,7 +64,7 @@
 #define R128_DEBUG          0   /* Turn off debugging output               */
 #define R128_IDLE_RETRY    32   /* Fall out of idle loops after this count */
 #define R128_TIMEOUT  2000000   /* Fall out of wait loops after this count */
-#define R128_MMIOSIZE 0x80000
+#define R128_MMIOSIZE  0x4000
 
 #define R128_VBIOS_SIZE 0x00010000
 
