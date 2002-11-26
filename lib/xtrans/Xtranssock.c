0b2968d9ad1cbcfeeac3aec0223c1d5bdee10dc3
dawes <dawes>
2002-11-26 01:12:30 +0000

    537. Fix a few cases where the transport endpoints would be removed       even if NOUNLINK is specified in the flags (#5484, J. Kean Johnston).

--- lib/xtrans/Xtranssock.c
+++ lib/xtrans/Xtranssock.c
@@ -26,7 +26,7 @@ other dealings in this Software without prior written authorization
 from The Open Group.
 
 */
-/* $XFree86: xc/lib/xtrans/Xtranssock.c,v 3.54 2002/05/31 18:45:51 dawes Exp $ */
+/* $XFree86: xc/lib/xtrans/Xtranssock.c,v 3.55 2002/11/20 23:00:36 dawes Exp $ */
 
 /* Copyright 1993, 1994 NCR Corporation - Dayton, Ohio, USA
  *
@@ -797,7 +797,7 @@ TRANS(SocketCreateListener) (XtransConnInfo ciptr,
 	
     /* Set a flag to indicate that this connection is a listener */
 
-    ciptr->flags = 1;
+    ciptr->flags = 1 | (ciptr->flags & TRANS_KEEPFLAGS);
 
     return 0;
 }
