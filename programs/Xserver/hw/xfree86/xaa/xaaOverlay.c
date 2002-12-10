ac5d6ec2b7dd6a4d4510b58ef5b88c2f3610bc0e
dawes <dawes>
2002-12-10 04:18:20 +0000

    fix build warning

--- programs/Xserver/hw/xfree86/xaa/xaaOverlay.c
+++ programs/Xserver/hw/xfree86/xaa/xaaOverlay.c
@@ -1,4 +1,4 @@
-/* $XFree86: xc/programs/Xserver/hw/xfree86/xaa/xaaOverlay.c,v 1.12 2000/09/28 20:48:01 mvojkovi Exp $ */
+/* $XFree86: xc/programs/Xserver/hw/xfree86/xaa/xaaOverlay.c,v 1.13 2001/10/28 03:34:04 tsi Exp $ */
 
 #include "misc.h"
 #include "xf86.h"
@@ -20,8 +20,6 @@
 #include "panoramiX.h"
 #include "panoramiXsrv.h"
 #endif
-
-extern WindowPtr *WindowTable;
 
 static void
 XAACopyWindow8_32(
