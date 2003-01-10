f15dc1e9fbed280490014c195e198d7095d78a3a
tsi <tsi>
2003-01-10 20:57:54 +0000

    708. Fix for when firmware/BIOS initialisation does not enable the extra       command FIFO entries available on second-generation integrated Mach64       variants (Marc La France).

--- programs/Xserver/hw/xfree86/drivers/ati/ativersion.h
+++ programs/Xserver/hw/xfree86/drivers/ati/ativersion.h
@@ -1,4 +1,4 @@
-/* $XFree86: xc/programs/Xserver/hw/xfree86/drivers/ati/ativersion.h,v 1.56 2002/12/17 15:45:26 tsi Exp $ */
+/* $XFree86: xc/programs/Xserver/hw/xfree86/drivers/ati/ativersion.h,v 1.57tsi Exp $ */
 /*
  * Copyright 1997 through 2003 by Marc Aurele La France (TSI @ UQV), tsi@xfree86.org
  *
@@ -39,7 +39,7 @@
 
 #define ATI_VERSION_MAJOR 6
 #define ATI_VERSION_MINOR 4
-#define ATI_VERSION_PATCH 17
+#define ATI_VERSION_PATCH 18
 
 #ifndef ATI_VERSION_EXTRA
 #define ATI_VERSION_EXTRA ""
