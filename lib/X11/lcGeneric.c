133cd6fa2b8d41df7c5600002abf7c8d85b1b002
paulo <paulo>
2003-02-03 22:11:58 +0000

    Fix another case of "pattern":   tmp = malloc(strlen(ptr + 1));   strcpy(tmp, ptr);

--- lib/X11/lcGeneric.c
+++ lib/X11/lcGeneric.c
@@ -28,7 +28,7 @@
  *  This is source code modified by FUJITSU LIMITED under the Joint
  *  Development Agreement for the CDE/Motif PST.
  */
-/* $XFree86: xc/lib/X11/lcGeneric.c,v 3.15 2001/07/25 15:04:45 dawes Exp $ */
+/* $XFree86: xc/lib/X11/lcGeneric.c,v 3.16 2001/11/16 00:52:27 dawes Exp $ */
 
 #include <stdio.h>
 #include "Xlibint.h"
@@ -526,7 +526,7 @@ read_charset_define(
                 Xfree(charsetd->encoding_name);
             }
 */
-            tmp = (char *)Xmalloc(strlen(value[0]+1));
+            tmp = (char *)Xmalloc(strlen(value[0])+1);
             strcpy(tmp,value[0]);
             charsetd->encoding_name = tmp;
             charsetd->xrm_encoding_name = XrmStringToQuark(tmp);
