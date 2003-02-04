9fa0dc39fdadcc169f2b7cf473acb0fe5a9b5316
dawes <dawes>
2003-02-04 03:49:33 +0000

    Fix a hang in multi-thread mode caused by a missing UnlockDisplay() in XkbGetMapChanges(). (Jose Luu).

--- lib/X11/XKBGetMap.c
+++ lib/X11/XKBGetMap.c
@@ -25,7 +25,7 @@ THE USE OR PERFORMANCE OF THIS SOFTWARE.
 
 ********************************************************/
 
-/* $XFree86: xc/lib/X11/XKBGetMap.c,v 1.5 2000/02/17 14:16:21 dawes Exp $ */
+/* $XFree86: xc/lib/X11/XKBGetMap.c,v 1.6 2001/01/17 19:41:48 dawes Exp $ */
 
 #define NEED_REPLIES
 #define NEED_EVENTS
@@ -974,6 +974,7 @@ XkbGetMapChanges(dpy,xkb,changes)
 	UnlockDisplay(dpy);
 	return status;
     }
+    UnlockDisplay(dpy);
     return Success;
 }
 
