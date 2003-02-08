06c2b3d0a455dd8bc65f7de8d2c64b601a313456
tsi <tsi>
2003-02-08 23:14:29 +0000

    Warning fix

--- programs/Xserver/hw/xfree86/drivers/nsc/nsc.h
+++ programs/Xserver/hw/xfree86/drivers/nsc/nsc.h
@@ -1,4 +1,4 @@
-/* $XFree86: xc/programs/Xserver/hw/xfree86/drivers/nsc/nsc.h,v 1.2 2002/12/11 22:50:59 dawes Exp $ */
+/* $XFree86: xc/programs/Xserver/hw/xfree86/drivers/nsc/nsc.h,v 1.3 2003/01/14 09:34:30 alanh Exp $ */
 /*
  * $Workfile: nsc.h $
  * $Revision$
@@ -296,7 +296,7 @@ typedef struct
    ScreenBlockHandlerProcPtr BlockHandler;	/* needed for video */
    int DetectedChipSet;
    int Chipset;
-   int FBLinearAddr;
+   unsigned long FBLinearAddr;
    unsigned char *FBBase;
    unsigned long FBSize;
    unsigned int cpu_reg_size;
