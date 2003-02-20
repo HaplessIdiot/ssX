38329398e37230d7dd6d046f406a98c7d895f523
dawes <dawes>
2003-02-20 02:59:02 +0000

    updates

--- programs/Xserver/hw/xfree86/xf86Version.h
+++ programs/Xserver/hw/xfree86/xf86Version.h
@@ -1,4 +1,4 @@
-/* $XFree86: xc/programs/Xserver/hw/xfree86/xf86Version.h,v 3.535 2003/02/17 04:06:32 dawes Exp $ */
+/* $XFree86: xc/programs/Xserver/hw/xfree86/xf86Version.h,v 3.536 2003/02/17 17:03:38 dawes Exp $ */
 
 #ifndef XF86_VERSION_CURRENT
 
@@ -29,7 +29,7 @@
 			0)
 
 
-#define XF86_DATE	"17 February 2003"
+#define XF86_DATE	"19 February 2003"
 
 #endif
 
