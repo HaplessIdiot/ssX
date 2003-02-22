4968b8554060ff270c05d49308bddb9c6d481683
dawes <dawes>
2003-02-22 06:21:11 +0000

    942. Correctly initialize the RE_LINE_PATTERN register in the radeon       and r200 DRI drivers (Keith Whitwell).

--- lib/GL/mesa/src/drv/r200/r200_state_init.c
+++ lib/GL/mesa/src/drv/r200/r200_state_init.c
@@ -1,4 +1,4 @@
-/* $XFree86: xc/lib/GL/mesa/src/drv/r200/r200_state_init.c,v 1.1 2002/10/30 12:51:52 alanh Exp $ */
+/* $XFree86: xc/lib/GL/mesa/src/drv/r200/r200_state_init.c,v 1.3 2003/01/29 23:00:41 dawes Exp $ */
 /*
 Copyright (C) The Weather Channel, Inc.  2002.  All Rights Reserved.
 
@@ -407,11 +407,7 @@ void r200InitState( r200ContextPtr rmesa )
    rmesa->hw.set.cmd[SET_RE_CNTL] = (R200_PERSPECTIVE_ENABLE |
 				     R200_SCISSOR_ENABLE);
 
-   rmesa->hw.lin.cmd[LIN_RE_LINE_PATTERN] =
-      ((0x0000 & R200_LINE_PATTERN_MASK) |
-       (0 << R200_LINE_REPEAT_COUNT_SHIFT) |
-       (0 << R200_LINE_PATTERN_START_SHIFT) |
-       R200_LINE_PATTERN_LITTLE_BIT_ORDER);
+   rmesa->hw.lin.cmd[LIN_RE_LINE_PATTERN] = ((1 << 16) | 0xffff);
 
    rmesa->hw.lin.cmd[LIN_RE_LINE_STATE] = 
       ((0 << R200_LINE_CURRENT_PTR_SHIFT) |
