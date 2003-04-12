6b019c9dbfee0bf26864ccb0270fab72b86b1168
dawes <dawes>
2003-04-12 17:18:13 +0000

    94. Rework the Linux drm kernel module build to leverage off the standard       kernel build system.  This is based on suggestions and examples from       David Woodhouse.  This approach has the advantage that the build       requirements of a wider range of standard kernels are now supported       transparently, but the disadvantage of some extra complexity to handle       building against clean vendor-distributed kernel source trees.       This has been tested with some recent Red Hat and SuSE distributions       (David Dawes).

--- programs/Xserver/hw/xfree86/xf86Date.h
+++ programs/Xserver/hw/xfree86/xf86Date.h
@@ -1,7 +1,7 @@
-/* $XFree86: xc/programs/Xserver/hw/xfree86/xf86Date.h,v 1.9 2003/04/10 13:26:41 dawes Exp $ */
+/* $XFree86: xc/programs/Xserver/hw/xfree86/xf86Date.h,v 1.10 2003/04/11 16:27:48 dawes Exp $ */
 
 #ifndef XF86_DATE
 
-#define XF86_DATE	"11 April 2003"
+#define XF86_DATE	"12 April 2003"
 
 #endif
