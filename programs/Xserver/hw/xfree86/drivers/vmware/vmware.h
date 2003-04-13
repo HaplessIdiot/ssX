4332c1a46387a7368c5a1a8a96899b70a9c872ee
dawes <dawes>
2003-04-13 18:09:27 +0000

    95. Fix vmware driver crash when running two X servers concurrently (#5688,       Nolan Leake).

--- programs/Xserver/hw/xfree86/drivers/vmware/vmware.h
+++ programs/Xserver/hw/xfree86/drivers/vmware/vmware.h
@@ -3,7 +3,7 @@
  * All Rights Reserved
  * Id: vmware.h,v 1.6 2001/01/30 18:13:47 bennett Exp $
  * **********************************************************/
-/* $XFree86: xc/programs/Xserver/hw/xfree86/drivers/vmware/vmware.h,v 1.9 2002/12/11 17:07:58 dawes Exp $ */
+/* $XFree86: xc/programs/Xserver/hw/xfree86/drivers/vmware/vmware.h,v 1.10 2003/02/04 01:39:53 dawes Exp $ */
 
 #ifndef VMWARE_H
 #define VMWARE_H
@@ -87,6 +87,7 @@ typedef struct {
 
 #ifdef RENDER
     CompositeProcPtr Composite;
+    void (*EnableDisableFBAccess)(int, Bool);
 #endif /* RENDER */
 
     unsigned long mmioPhysBase;
