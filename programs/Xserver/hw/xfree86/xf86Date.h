bd8749809f5a53aac4a2486688535b3b4ec4bc02
dawes <dawes>
2003-04-15 03:15:55 +0000

    101. Change the WORLDOPTS default to empty.  This means that 'make World'       will now stop on the first error.  To get the old behaviour, run       'make WORLDOPTS=-k World' (David Dawes).

--- programs/Xserver/hw/xfree86/xf86Date.h
+++ programs/Xserver/hw/xfree86/xf86Date.h
@@ -1,7 +1,7 @@
-/* $XFree86: xc/programs/Xserver/hw/xfree86/xf86Date.h,v 1.11 2003/04/12 17:18:16 dawes Exp $ */
+/* $XFree86: xc/programs/Xserver/hw/xfree86/xf86Date.h,v 1.12 2003/04/13 19:27:48 dawes Exp $ */
 
 #ifndef XF86_DATE
 
-#define XF86_DATE	"13 April 2003"
+#define XF86_DATE	"14 April 2003"
 
 #endif
