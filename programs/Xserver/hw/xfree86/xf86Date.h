a08eef57669bb7a28f4f7d760783e76bb515af03
dawes <dawes>
2003-04-16 15:46:08 +0000

    106. Fix DRM module build on 2.5.41 and later kernels (tqueue -> workqueue)       (David Dawes).

--- programs/Xserver/hw/xfree86/xf86Date.h
+++ programs/Xserver/hw/xfree86/xf86Date.h
@@ -1,7 +1,7 @@
-/* $XFree86: xc/programs/Xserver/hw/xfree86/xf86Date.h,v 1.12 2003/04/13 19:27:48 dawes Exp $ */
+/* $XFree86: xc/programs/Xserver/hw/xfree86/xf86Date.h,v 1.13 2003/04/15 03:16:01 dawes Exp $ */
 
 #ifndef XF86_DATE
 
-#define XF86_DATE	"14 April 2003"
+#define XF86_DATE	"16 April 2003"
 
 #endif
