6ed5c51c369b8a0be7bff49cda6e996ab535413f
dawes <dawes>
2003-04-25 16:09:40 +0000

    Snapshot: 4.3.99.3

--- programs/Xserver/hw/xfree86/xf86Date.h
+++ programs/Xserver/hw/xfree86/xf86Date.h
@@ -1,7 +1,7 @@
-/* $XFree86: xc/programs/Xserver/hw/xfree86/xf86Date.h,v 1.17 2003/04/24 02:58:08 dawes Exp $ */
+/* $XFree86: xc/programs/Xserver/hw/xfree86/xf86Date.h,v 1.18 2003/04/25 00:57:22 dawes Exp $ */
 
 #ifndef XF86_DATE
 
-#define XF86_DATE	"24 April 2003"
+#define XF86_DATE	"25 April 2003"
 
 #endif
--- programs/Xserver/hw/xfree86/xf86Version.h
+++ programs/Xserver/hw/xfree86/xf86Version.h
@@ -1,11 +1,11 @@
-/* $XFree86: xc/programs/Xserver/hw/xfree86/xf86Version.h,v 3.544 2003/03/24 04:17:28 dawes Exp $ */
+/* $XFree86: xc/programs/Xserver/hw/xfree86/xf86Version.h,v 3.545 2003/04/10 13:26:41 dawes Exp $ */
 
 #ifndef XF86_VERSION_CURRENT
 
 #define XF86_VERSION_MAJOR	4
 #define XF86_VERSION_MINOR	3
 #define XF86_VERSION_PATCH	99
-#define XF86_VERSION_SNAP	2
+#define XF86_VERSION_SNAP	3
 
 /* This has five arguments for compatibilty reasons */
 #define XF86_VERSION_NUMERIC(major,minor,patch,snap,dummy) \
