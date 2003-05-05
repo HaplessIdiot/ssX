2f6bac100662cef4eb6123d9e51b007b64bf2144
dawes <dawes>
2003-05-05 16:12:27 +0000

    159. Make freetype-config more useful on NetBSD (#5693, Thomas Klausner).

--- programs/Xserver/hw/xfree86/xf86Date.h
+++ programs/Xserver/hw/xfree86/xf86Date.h
@@ -1,7 +1,7 @@
-/* $XFree86: xc/programs/Xserver/hw/xfree86/xf86Date.h,v 1.20 2003/04/28 17:07:58 dawes Exp $ */
+/* $XFree86: xc/programs/Xserver/hw/xfree86/xf86Date.h,v 1.21 2003/04/29 15:11:30 dawes Exp $ */
 
 #ifndef XF86_DATE
 
-#define XF86_DATE	"29 April 2003"
+#define XF86_DATE	"5 May 2003"
 
 #endif
