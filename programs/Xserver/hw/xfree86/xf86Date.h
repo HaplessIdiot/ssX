dfc87863dbce1562a68c2c037d6766ee89afce61
dawes <dawes>
2003-05-07 21:56:31 +0000

    165. Check for NULL tObj in the i830 3D driver's TexEnv function.  This       fixes a FlightGear crash (Keith Whitwell).

--- programs/Xserver/hw/xfree86/xf86Date.h
+++ programs/Xserver/hw/xfree86/xf86Date.h
@@ -1,7 +1,7 @@
-/* $XFree86: xc/programs/Xserver/hw/xfree86/xf86Date.h,v 1.21 2003/04/29 15:11:30 dawes Exp $ */
+/* $XFree86: xc/programs/Xserver/hw/xfree86/xf86Date.h,v 1.22 2003/05/05 16:12:32 dawes Exp $ */
 
 #ifndef XF86_DATE
 
-#define XF86_DATE	"5 May 2003"
+#define XF86_DATE	"7 May 2003"
 
 #endif
