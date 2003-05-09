37a3b2179bbbf3a74a9443d1be85500e5d920309
dawes <dawes>
2003-05-09 03:01:42 +0000

    updates

--- programs/Xserver/hw/xfree86/xf86Date.h
+++ programs/Xserver/hw/xfree86/xf86Date.h
@@ -1,7 +1,7 @@
-/* $XFree86: xc/programs/Xserver/hw/xfree86/xf86Date.h,v 1.22 2003/05/05 16:12:32 dawes Exp $ */
+/* $XFree86: xc/programs/Xserver/hw/xfree86/xf86Date.h,v 1.23 2003/05/07 21:56:33 dawes Exp $ */
 
 #ifndef XF86_DATE
 
-#define XF86_DATE	"7 May 2003"
+#define XF86_DATE	"8 May 2003"
 
 #endif
