b18bec271430707b23df6ce25a2e77377a977bb4
tsi <tsi>
2003-05-23 14:58:02 +0000

    Signedness glitch

--- programs/xedit/util.c
+++ programs/xedit/util.c
@@ -24,7 +24,7 @@
  * used in advertising or publicity pertaining to distribution of the software
  * without specific, written prior permission.
  */
-/* $XFree86: xc/programs/xedit/util.c,v 1.25 2002/12/04 05:27:56 paulo Exp $ */
+/* $XFree86: xc/programs/xedit/util.c,v 1.26 2003/05/07 20:54:43 herrb Exp $ */
 
 #include <stdio.h>
 #include <stdlib.h>		/* for realpath() */
@@ -234,7 +234,7 @@ KillTextSource(xedit_flist_item *item)
 	if (flist.itens[idx] == item) {
 	    if (idx + 1 < flist.num_itens)
 		nitem = flist.itens[idx + 1];
-	    else if (idx - 1 >= 0)
+	    else if (idx >= 1)
 		nitem = flist.itens[idx - 1];
 	    break;
 	}
