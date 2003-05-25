d1f5c60ee206aad3e5fdc84e06d0faa7a9426bfa
dawes <dawes>
2003-05-25 01:27:37 +0000

    185. Fix libexpat shared lib revision on platforms that use minor version       numbers (David Dawes). + use 'sed' instead of 'expr' to get the major version to avoid problems   with the non-zero exit status of expr when the the major version is 0.

--- programs/Xserver/hw/xfree86/xf86Date.h
+++ programs/Xserver/hw/xfree86/xf86Date.h
@@ -1,7 +1,7 @@
-/* $XFree86: xc/programs/Xserver/hw/xfree86/xf86Date.h,v 1.26 2003/05/20 21:59:30 dawes Exp $ */
+/* $XFree86: xc/programs/Xserver/hw/xfree86/xf86Date.h,v 1.27 2003/05/22 17:56:14 dawes Exp $ */
 
 #ifndef XF86_DATE
 
-#define XF86_DATE	"22 May 2003"
+#define XF86_DATE	"24 May 2003"
 
 #endif
