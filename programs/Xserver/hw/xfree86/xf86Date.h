60edd53fc07667fd0dd59c8c22f3662f00b34a99
dawes <dawes>
2003-06-09 15:10:58 +0000

    206. Import fontconfig 2.1.0 as an external package, and update the build       to use this version (David Dawes).

--- programs/Xserver/hw/xfree86/xf86Date.h
+++ programs/Xserver/hw/xfree86/xf86Date.h
@@ -1,7 +1,7 @@
-/* $XFree86: xc/programs/Xserver/hw/xfree86/xf86Date.h,v 1.29 2003/05/25 11:06:39 dawes Exp $ */
+/* $XFree86: xc/programs/Xserver/hw/xfree86/xf86Date.h,v 1.30 2003/05/29 03:05:11 dawes Exp $ */
 
 #ifndef XF86_DATE
 
-#define XF86_DATE	"28 May 2003"
+#define XF86_DATE	"9 June 2003"
 
 #endif
