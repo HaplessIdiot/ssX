a19575a89ca77995147c9ed82d64b36e2b7025eb
dawes <dawes>
2003-06-12 17:25:09 +0000

    215. Fix i810/i815 problem with interlaced modes where only the top half       of the screen gets displayed (Paul Stewart).

--- programs/Xserver/hw/xfree86/xf86Date.h
+++ programs/Xserver/hw/xfree86/xf86Date.h
@@ -1,7 +1,7 @@
-/* $XFree86: xc/programs/Xserver/hw/xfree86/xf86Date.h,v 1.31 2003/06/09 15:11:15 dawes Exp $ */
+/* $XFree86: xc/programs/Xserver/hw/xfree86/xf86Date.h,v 1.32 2003/06/10 11:08:37 dawes Exp $ */
 
 #ifndef XF86_DATE
 
-#define XF86_DATE	"10 June 2003"
+#define XF86_DATE	"12 June 2003"
 
 #endif
