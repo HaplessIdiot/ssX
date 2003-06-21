74330ae2524b7dd6aae5d3ad5a3fc5667bee924f
twini <twini>
2003-06-21 16:05:11 +0000

    Fix for Compaq Presario 3045US

--- programs/Xserver/hw/xfree86/drivers/sis/sis_driver.c
+++ programs/Xserver/hw/xfree86/drivers/sis/sis_driver.c
@@ -1442,7 +1442,7 @@ SISPreInit(ScrnInfoPtr pScrn, int flags)
     xf86LoaderReqSymLists(vgahwSymbols, NULL);
 
     xf86DrvMsg(pScrn->scrnIndex, X_INFO,
-        "SiS driver (2003/06/18-1) by "
+        "SiS driver (2003/06/21-1) by "
 	"Thomas Winischhofer <thomas@winischhofer.net>\n");
     xf86DrvMsg(pScrn->scrnIndex, X_INFO,
         "See http://www.winischhofer.net/linuxsisvga.shtml "
