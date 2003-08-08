281ca1ee2840006e048a7a903c8f7edbc23bf3f6
dawes <dawes>
2003-08-08 06:27:05 +0000

    360. Add UK (gb) layout to the dvorak XKB map (#5739, Mike Sulivan).

--- programs/Xserver/hw/xfree86/xf86Date.h
+++ programs/Xserver/hw/xfree86/xf86Date.h
@@ -1,7 +1,7 @@
-/* $XFree86: xc/programs/Xserver/hw/xfree86/xf86Date.h,v 1.42 2003/07/29 21:32:59 dawes Exp $ */
+/* $XFree86: xc/programs/Xserver/hw/xfree86/xf86Date.h,v 1.43 2003/08/04 04:22:12 dawes Exp $ */
 
 #ifndef XF86_DATE
 
-#define XF86_DATE	"4 August 2003"
+#define XF86_DATE	"8 August 2003"
 
 #endif
