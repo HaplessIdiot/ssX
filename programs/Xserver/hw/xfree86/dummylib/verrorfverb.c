5f6a6dd1f4bd79008187ee66174483a5506fda7e
dawes <dawes>
2003-08-25 04:13:05 +0000

    warning fix

--- programs/Xserver/hw/xfree86/dummylib/verrorfverb.c
+++ programs/Xserver/hw/xfree86/dummylib/verrorfverb.c
@@ -1,9 +1,10 @@
-/* $XFree86$ */
+/* $XFree86: xc/programs/Xserver/hw/xfree86/dummylib/verrorfverb.c,v 1.1 2000/02/13 03:06:39 dawes Exp $ */
 
 #include "X.h"
 #include "os.h"
 #include "xf86.h"
 #include "xf86Priv.h"
+#include "dummylib.h"
 
 /*
  * Utility functions required by libxf86_os. 
