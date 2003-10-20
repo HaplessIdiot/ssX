deabf7392771ae24f41e483eaf9e23278aeed38f
dickey <dickey>
2003-10-20 00:58:54 +0000

    Development - 2003/10/19 - XFree86 4.3.99.15
         * modify  xtermAddInput()  (see  note  on augmentation in [184]patch
           #158)  to use the complete set of default keyboard translations so
           that  one  can  use  shifted  pageup, wheel mouse, etc., while the
           mouse pointer is over the scrollbar. (Debian #178812).
         * make save/restore mode controls apply to show/blink cursor states.
         * add  escape  sequence  to start/stop blinking cursor, which allows
           implementing cvvis terminfo capability (request by Nate Bargmann).
         * add indp and rin to terminfo entry.
         * fix  an  out-of-bounds  array  reference in ScrnRefresh() for wide
           characters (report by Dan Harnett, patch by Todd Miller, bug dates
           from [185]patch #141).

--- programs/xterm/input.c
+++ programs/xterm/input.c
@@ -2,7 +2,7 @@
  *	$Xorg: input.c,v 1.3 2000/08/17 19:55:08 cpqbld Exp $
  */
 
-/* $XFree86: xc/programs/xterm/input.c,v 3.63 2003/05/19 00:47:32 dickey Exp $ */
+/* $XFree86: xc/programs/xterm/input.c,v 3.64 2003/09/21 17:12:47 dickey Exp $ */
 
 /*
  * Copyright 1999-2001,2002 by Thomas E. Dickey
@@ -325,36 +325,6 @@ xtermDeleteIsDEL(void)
 	   result));
 
     return result;
-}
-
-/*
- * Add input-actions for widgets that are overlooked (scrollbar and toolbar):
- *
- *	a) Sometimes the scrollbar passes through translations, sometimes it
- *	   doesn't.  We add the KeyPress translations here, just to be sure.
- *	b) In the normal (non-toolbar) configuration, the xterm widget covers
- *	   almost all of the window.  With a toolbar, there's a relatively
- *	   large area that the user would expect to enter keystrokes since the
- *	   program can get the focus.
- */
-void
-xtermAddInput(Widget w)
-{
-    static char input_trans[] = "\
-                ~Meta <KeyPress>:insert-seven-bit() \n\
-                 Meta <KeyPress>:insert-eight-bit() \n";
-    /* *INDENT-OFF* */
-    XtActionsRec input_actions[] = {
-	{ "insert",		HandleKeyPressed }, /* alias */
-	{ "insert-eight-bit",	HandleEightBitKeyPressed },
-	{ "insert-seven-bit",	HandleKeyPressed },
-	{ "secure",		HandleSecure },
-	{ "string",		HandleStringEvent },
-    };
-    /* *INDENT-ON* */
-
-    XtAppAddActions(app_con, input_actions, XtNumber(input_actions));
-    XtAugmentTranslations(w, XtParseTranslationTable(input_trans));
 }
 
 void
@@ -1047,6 +1017,7 @@ TranslationsUseKeyword(Widget w, const char *keyword)
 
     if (data != 0) {
 	char *p = data;
+	TRACE(("TranslationsUseKeyword(%p):%s\n", w, p));
 	int state = 0;
 	int now = ' ', prv;
 	while (*p != 0) {
@@ -1073,6 +1044,7 @@ TranslationsUseKeyword(Widget w, const char *keyword)
 	    }
 	}
     }
+    TRACE(("TranslationsUseKeyword(%p, %s) = %d\n", w, keyword, result));
     return result;
 }
 
