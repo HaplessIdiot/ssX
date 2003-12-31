c415970eee1cc42d3ea59763dfb7c1ee33b060e0
dawes <dawes>
2003-12-31 05:44:08 +0000

    715. Fix a long standing VIA Xv problem with totem/xine and YUV surface       types (Uberto Barbini).

--- programs/Xserver/hw/xfree86/drivers/via/via_video.c
+++ programs/Xserver/hw/xfree86/drivers/via/via_video.c
@@ -1,4 +1,4 @@
-/* $XFree86: xc/programs/Xserver/hw/xfree86/drivers/via/via_video.c,v 1.12 2003/11/10 18:22:35 tsi Exp $ */
+/* $XFree86: xc/programs/Xserver/hw/xfree86/drivers/via/via_video.c,v 1.13 2003/12/31 05:42:05 dawes Exp $ */
 /*
  * Copyright 1998-2003 VIA Technologies, Inc. All Rights Reserved.
  * Copyright 2001-2003 S3 Graphics, Inc. All Rights Reserved.
@@ -906,6 +906,10 @@ static void CopyDataYUV422(
     /*  copy YUY2 data to video memory,
      *  do 32 bits alignment.
      */
+     
+    w <<= 1;		/* Each pixel is YU or YV - 16 bits */
+    srcPitch <<= 1;
+    
     count = h;
     while(count--) {
         memcpy(dst, src, w);
