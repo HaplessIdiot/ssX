7e2b694a7dae0f418dc22b62d248e73db324c8c5
alanh <alanh>
2004-04-08 09:15:14 +0000

    Initial revision

---
+++ extras/Mesa/src/glu/sgi/dummy.cc
@@ -0,0 +1,5 @@
+/* $Id: dummy.cc,v 1.1 2004/04/08 09:16:09 alanh Exp $ */
+/*
+ * This file contains nothing. It's just there so there's at least a single
+ * source file for libGLU.la in this directory.
+ */
