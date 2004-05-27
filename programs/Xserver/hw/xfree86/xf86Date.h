3e0376d954a6fccb6a1d92551c89295ec426246d
dawes <dawes>
2004-05-27 11:02:20 +0000

    Snapshot: 4.4.99.6

--- programs/Xserver/hw/xfree86/xf86Date.h
+++ programs/Xserver/hw/xfree86/xf86Date.h
@@ -1,4 +1,4 @@
-/* $XFree86: xc/programs/Xserver/hw/xfree86/xf86Date.h,v 1.62 2004/04/27 11:02:41 dawes Exp $ */
+/* $XFree86: xc/programs/Xserver/hw/xfree86/xf86Date.h,v 1.63 2004/05/12 11:02:39 dawes Exp $ */
 /*
  * Copyright (c) 2004 by The XFree86 Project, Inc.
  * All rights reserved.
@@ -48,6 +48,6 @@
 
 #ifndef XF86_DATE
 
-#define XF86_DATE	"12 May 2004"
+#define XF86_DATE	"27 May 2004"
 
 #endif
