f319d770afc01100fbb79a3bbad924e7fa397d35
dawes <dawes>
2004-06-08 01:28:54 +0000

    125. Add support for the NEC PC-9821 with CLGD755x to the Cirrus driver,       and update the relevant Japanese docs (Satoshi Kimura).  124. Don't do various keycode conversions on NEC PC-98 platforms       (Satoshi Kimura).

--- programs/Xserver/hw/xfree86/drivers/cirrus/cir_driver.c
+++ programs/Xserver/hw/xfree86/drivers/cirrus/cir_driver.c
@@ -11,7 +11,7 @@
  *	Guy DESBIEF
  */
 
-/* $XFree86: xc/programs/Xserver/hw/xfree86/drivers/cirrus/cir_driver.c,v 1.67 2001/05/15 10:19:37 eich Exp $ */
+/* $XFree86: xc/programs/Xserver/hw/xfree86/drivers/cirrus/cir_driver.c,v 1.69 2003/06/18 16:17:38 eich Exp $ */
 
 /* All drivers should typically include these */
 #include "xf86.h"
@@ -86,6 +86,8 @@ SymTabRec CIRChipsets[] = {
 	{ PCI_CHIP_GD5464BD,	"CL-GD5464BD" },
 	{ PCI_CHIP_GD5465,		"CL-GD5465" },
 	{ PCI_CHIP_GD7548,              "CL-GD7548" },
+	{ PCI_CHIP_GD7555,              "CL-GD7555" },
+	{ PCI_CHIP_GD7556,              "CL-GD7556" },
 	{-1,					NULL }
 };
 
@@ -103,6 +105,8 @@ PciChipsets CIRPciChipsets[] = {
 	{ PCI_CHIP_GD5464BD,PCI_CHIP_GD5464BD,	RES_SHARED_VGA },
 	{ PCI_CHIP_GD5465,	PCI_CHIP_GD5465,	RES_SHARED_VGA },
 	{ PCI_CHIP_GD7548,	PCI_CHIP_GD7548,	RES_SHARED_VGA },
+	{ PCI_CHIP_GD7555,	PCI_CHIP_GD7555,	RES_SHARED_VGA },
+	{ PCI_CHIP_GD7556,	PCI_CHIP_GD7556,	RES_SHARED_VGA },
 	{ -1,				-1,					RES_UNDEFINED}
 };
 
