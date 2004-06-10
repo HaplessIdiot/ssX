c7a5bc2abc3296e13b46df9744abcbc67dc08554
alanh <alanh>
2004-06-10 13:08:27 +0000

    131. Intel driver updates to support:-        - i915 latest generation hardware        - Dual Head & Clone support for i915, i855 and i830 chipsets.        - ARGB cursor support for all Intel chipsets (requires agpgart updates)        - Mobile lid status checking        - Fix XVideo when high resolutions are used on LFP's

--- programs/Xserver/hw/xfree86/drivers/i810/i810_dri.c
+++ programs/Xserver/hw/xfree86/drivers/i810/i810_dri.c
@@ -1,4 +1,4 @@
-/* $XFree86: xc/programs/Xserver/hw/xfree86/drivers/i810/i810_dri.c,v 1.41 2003/09/24 02:43:23 dawes Exp $ */
+/* $XFree86: xc/programs/Xserver/hw/xfree86/drivers/i810/i810_dri.c,v 1.42 2003/09/28 20:15:58 alanh Exp $ */
 /*
  * Reformatted with GNU indent (2.2.8), using the following options:
  *
@@ -104,7 +104,7 @@ Bool
 I810InitDma(ScrnInfoPtr pScrn)
 {
    I810Ptr pI810 = I810PTR(pScrn);
-   I810RingBuffer *ring = &(pI810->LpRing);
+   I810RingBuffer *ring = pI810->LpRing;
    I810DRIPtr pI810DRI = (I810DRIPtr) pI810->pDRIInfo->devPrivate;
    drmI810Init info;
 
@@ -514,6 +514,7 @@ I810DRIScreenInit(ScreenPtr pScreen)
    memset(&pI810->BackBuffer, 0, sizeof(I810MemRange));
    memset(&pI810->DepthBuffer, 0, sizeof(I810MemRange));
    pI810->CursorPhysical = 0;
+   pI810->CursorARGBPhysical = 0;
 
    /* Dcache - half the speed of normal ram, but has use as a Z buffer
     * under the DRI.
@@ -587,6 +588,7 @@ I810DRIScreenInit(ScreenPtr pScreen)
    }
 
    sysmem_size -= 4096;			/* remove 4k for the hw cursor */
+   sysmem_size -= 16384;		/* remove 16k for the ARGB hw cursor */
 
    pI810->SysMem.Start = 0;
    pI810->SysMem.Size = sysmem_size;
@@ -765,6 +767,30 @@ I810DRIScreenInit(ScreenPtr pScreen)
       pI810->CursorPhysical = 0;
    }
 
+   drmAgpAlloc(pI810->drmSubFD, 16384, 2,
+	       (unsigned long *)&pI810->CursorARGBPhysical, &agpHandle);
+
+   pI810->cursorARGBHandle = agpHandle;
+
+   if (agpHandle != DRM_AGP_NO_HANDLE) {
+ 	int r;
+
+      if ((r = drmAgpBind(pI810->drmSubFD, agpHandle, tom)) == 0) {
+	 xf86DrvMsg(pScrn->scrnIndex, X_INFO,
+		    "[agp] GART: Allocated 16K for ARGB mouse cursor image\n");
+	 pI810->CursorARGBStart = tom;
+	 tom += 16384;
+      } else {
+	 xf86DrvMsg(pScrn->scrnIndex, X_INFO,
+		    "[agp] GART: ARGB cursor bind failed\n");
+	 pI810->CursorARGBPhysical = 0;
+      }
+   } else {
+      xf86DrvMsg(pScrn->scrnIndex, X_INFO,
+		 "[agp] GART: ARGB cursor alloc failed\n");
+      pI810->CursorARGBPhysical = 0;
+   }
+
    /* Steal some of the excess cursor space for the overlay regs.
     */
    pI810->OverlayPhysical = pI810->CursorPhysical + 1024;
@@ -869,8 +895,8 @@ I810DRIScreenInit(ScreenPtr pScreen)
    pI810DRI->agp_buffers = pI810->buffer_map;
    pI810DRI->agp_buf_size = pI810->BufferMem.Size;
 
-   if (drmAddMap(pI810->drmSubFD, (drmHandle) pI810->LpRing.mem.Start,
-		 pI810->LpRing.mem.Size, DRM_AGP, 0, &pI810->ring_map) < 0) {
+   if (drmAddMap(pI810->drmSubFD, (drmHandle) pI810->LpRing->mem.Start,
+		 pI810->LpRing->mem.Size, DRM_AGP, 0, &pI810->ring_map) < 0) {
       xf86DrvMsg(pScreen->myNum, X_ERROR,
 		 "[drm] drmAddMap(ring_map) failed.  Disabling DRI.\n");
       DRICloseScreen(pScreen);
@@ -960,8 +986,8 @@ I810DRIScreenInit(ScreenPtr pScreen)
    pI810DRI->backOffset = pI810->BackBuffer.Start;
    pI810DRI->depthOffset = pI810->DepthBuffer.Start;
 
-   pI810DRI->ringOffset = pI810->LpRing.mem.Start;
-   pI810DRI->ringSize = pI810->LpRing.mem.Size;
+   pI810DRI->ringOffset = pI810->LpRing->mem.Start;
+   pI810DRI->ringSize = pI810->LpRing->mem.Size;
 
    pI810DRI->auxPitch = pI810->auxPitch;
    pI810DRI->auxPitchBits = pI810->auxPitchBits;
@@ -1500,6 +1526,10 @@ I810DRIEnter(ScrnInfoPtr pScrn)
       if (pI810->cursorHandle != 0)
 	 if (drmAgpBind(pI810->drmSubFD, pI810->cursorHandle,
 			pI810->CursorStart) != 0)
+	    return FALSE;
+      if (pI810->cursorARGBHandle != 0)
+	 if (drmAgpBind(pI810->drmSubFD, pI810->cursorARGBHandle,
+			pI810->CursorARGBStart) != 0)
 	    return FALSE;
    }
    return TRUE;
