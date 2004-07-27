05cf4b63408cd7d9bf15773ae6f61193a6c8c526
dawes <dawes>
2004-07-27 11:02:46 +0000

    Snapshot: 4.4.99.10

--- programs/Xserver/hw/xfree86/xf86Version.h
+++ programs/Xserver/hw/xfree86/xf86Version.h
@@ -1,4 +1,4 @@
-/* $XFree86: xc/programs/Xserver/hw/xfree86/xf86Version.h,v 3.579 2004/06/27 11:02:27 dawes Exp $ */
+/* $XFree86: xc/programs/Xserver/hw/xfree86/xf86Version.h,v 3.580 2004/07/12 11:02:36 dawes Exp $ */
 
 /*
  * Copyright (c) 1994-2004 by The XFree86 Project, Inc.
@@ -52,7 +52,7 @@
 #define XF86_VERSION_MAJOR	4
 #define XF86_VERSION_MINOR	4
 #define XF86_VERSION_PATCH	99
-#define XF86_VERSION_SNAP	9
+#define XF86_VERSION_SNAP	10
 
 /* This has five arguments for compatibilty reasons */
 #define XF86_VERSION_NUMERIC(major,minor,patch,snap,dummy) \
