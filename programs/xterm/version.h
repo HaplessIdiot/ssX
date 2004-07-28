85ee4938fe8ad9cbb9bba5d742002231a1fabf05
dickey <dickey>
2004-07-28 00:53:24 +0000

    163. Xterm patch #194 (Thomas Dickey).

--- programs/xterm/version.h
+++ programs/xterm/version.h
@@ -1,5 +1,5 @@
-/* $XTermId: version.h,v 1.226 2004/07/14 23:29:49 tom Exp $ */
-/* $XFree86: xc/programs/xterm/version.h,v 3.103 2004/07/13 00:41:30 dickey Exp $ */
+/* $XTermId: version.h,v 1.228 2004/07/27 23:30:39 tom Exp $ */
+/* $XFree86: xc/programs/xterm/version.h,v 3.104 2004/07/20 01:14:41 dickey Exp $ */
 
 /*
  * These definitions are used to build the string that's printed in response to
@@ -7,7 +7,7 @@
  * version of X to which this version of xterm has been built.  The number in
  * parentheses is my patch number (T.Dickey).
  */
-#define XTERM_PATCH   193
+#define XTERM_PATCH   194
 
 #ifndef __vendorversion__
 #define __vendorversion__ "XTerm"
