d789896f554cad02ea9c8cd23f835dd3d7505553
tsi <tsi>
2004-10-26 22:07:39 +0000

    RCS tags for recently modified files

--- extras/Mesa/src/mesa/swrast/s_alphabuf.c
+++ extras/Mesa/src/mesa/swrast/s_alphabuf.c
@@ -21,7 +21,7 @@
  * AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
  * CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
  */
-
+/* $XFree86$ */
 
 /*
  * Software alpha planes.  Many frame buffers don't have alpha bits so
