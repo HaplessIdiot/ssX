8289440106fcec74f2cc40c456f3ffc0a225157b
dawes <dawes>
2005-04-08 11:02:48 +0000

    Snapshot: 4.5.99.1

--- programs/Xserver/hw/xfree86/xf86Date.h
+++ programs/Xserver/hw/xfree86/xf86Date.h
@@ -1,4 +1,4 @@
-/* $XFree86: xc/programs/Xserver/hw/xfree86/xf86Date.h,v 1.85 2005/03/07 17:32:40 dawes Exp $ */
+/* $XFree86: xc/programs/Xserver/hw/xfree86/xf86Date.h,v 1.86 2005/03/17 01:11:53 dawes Exp $ */
 /*
  * Copyright (c) 2004, 2005 by The XFree86 Project, Inc.
  * All rights reserved.
@@ -48,6 +48,6 @@
 
 #ifndef XF86_DATE
 
-#define XF86_DATE	"16 March 2005"
+#define XF86_DATE	"8 April 2005"
 
 #endif
--- programs/Xserver/hw/xfree86/xf86Version.h
+++ programs/Xserver/hw/xfree86/xf86Version.h
@@ -1,4 +1,4 @@
-/* $XFree86: xc/programs/Xserver/hw/xfree86/xf86Version.h,v 3.598 2005/03/06 19:48:33 dawes Exp $ */
+/* $XFree86: xc/programs/Xserver/hw/xfree86/xf86Version.h,v 3.599 2005/03/17 01:11:53 dawes Exp $ */
 
 /*
  * Copyright (c) 1994-2005 by The XFree86 Project, Inc.
@@ -51,8 +51,8 @@
 
 #define XF86_VERSION_MAJOR	4
 #define XF86_VERSION_MINOR	5
-#define XF86_VERSION_PATCH	0
-#define XF86_VERSION_SNAP	0
+#define XF86_VERSION_PATCH	99
+#define XF86_VERSION_SNAP	1
 
 /* This has five arguments for compatibilty reasons */
 #define XF86_VERSION_NUMERIC(major, minor, patch, snap, dummy) \
