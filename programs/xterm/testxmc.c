6e5a8f2a43a1638037997203b294e039e76317c5
dickey <dickey>
2005-08-05 01:25:38 +0000

    106. Xterm patch #204 (Thomas Dickey).

--- programs/xterm/testxmc.c
+++ programs/xterm/testxmc.c
@@ -1,7 +1,7 @@
-/* $XTermId: testxmc.c,v 1.27 2005/01/10 22:01:49 tom Exp $ */
+/* $XTermId: testxmc.c,v 1.29 2005/07/23 15:07:47 tom Exp $ */
 
 /*
- * $XFree86: xc/programs/xterm/testxmc.c,v 3.11 2004/12/01 01:27:47 dickey Exp $
+ * $XFree86: xc/programs/xterm/testxmc.c,v 3.12 2005/01/14 01:50:03 dickey Exp $
  */
 
 /************************************************************
@@ -221,6 +221,6 @@ Resolve_XMC(TScreen * screen)
 
     if (changed) {
 	ScrnUpdate(screen, screen->cur_row, 0, row + 1 - screen->cur_row,
-		   screen->max_col + 1, True);
+		   MaxCols(screen), True);
     }
 }
--- programs/xterm/version.h
+++ programs/xterm/version.h
@@ -1,5 +1,5 @@
-/* $XTermId: version.h,v 1.245 2005/06/04 23:29:49 tom Exp $ */
-/* $XFree86: xc/programs/xterm/version.h,v 3.113 2005/05/03 00:38:25 dickey Exp $ */
+/* $XTermId: version.h,v 1.247 2005/07/18 00:24:40 tom Exp $ */
+/* $XFree86: xc/programs/xterm/version.h,v 3.114 2005/07/07 00:46:14 dickey Exp $ */
 
 /*
  * These definitions are used to build the string that's printed in response to
@@ -7,7 +7,7 @@
  * version of X to which this version of xterm has been built.  The number in
  * parentheses is my patch number (T.Dickey).
  */
-#define XTERM_PATCH   203
+#define XTERM_PATCH   204
 
 #ifndef __vendorversion__
 #define __vendorversion__ "XTerm"
