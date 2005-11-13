46e345a21d0d215f393398f3e1b87ef9adc916d0
dickey <dickey>
2005-11-13 23:10:33 +0000

    167. Xterm patch #207 (Thomas Dickey).

--- programs/xterm/VTparse.h
+++ programs/xterm/VTparse.h
@@ -1,12 +1,12 @@
-/* $XTermId: VTparse.h,v 1.35 2005/06/23 21:29:07 tom Exp $ */
+/* $XTermId: VTparse.h,v 1.37 2005/11/13 19:28:00 tom Exp $ */
 
 /*
  *	$Xorg: VTparse.h,v 1.3 2000/08/17 19:55:08 cpqbld Exp $
  */
 
-/* $XFree86: xc/programs/xterm/VTparse.h,v 3.20 2004/12/01 01:27:46 dickey Exp $ */
+/* $XFree86: xc/programs/xterm/VTparse.h,v 3.21 2005/07/07 00:46:13 dickey Exp $ */
 /*
- * Copyright 2002-2003,2004 by Thomas E. Dickey
+ * Copyright 2002-2004,2005 by Thomas E. Dickey
  *
  *                         All Rights Reserved
  *
