1fbf278ae901feda9364fab7c41f9823cabcb327
tsi <tsi>
2006-01-05 18:55:28 +0000

    The annual update of my copyright

--- programs/Xserver/hw/xfree86/drivers/ati/atidsp.h
+++ programs/Xserver/hw/xfree86/drivers/ati/atidsp.h
@@ -1,6 +1,6 @@
-/* $XFree86: xc/programs/Xserver/hw/xfree86/drivers/ati/atidsp.h,v 1.11 2004/01/05 16:42:02 tsi Exp $ */
+/* $XFree86: xc/programs/Xserver/hw/xfree86/drivers/ati/atidsp.h,v 1.12tsi Exp $ */
 /*
- * Copyright 1997 through 2005 by Marc Aurele La France (TSI @ UQV), tsi@xfree86.org
+ * Copyright 1997 through 2006 by Marc Aurele La France (TSI @ UQV), tsi@xfree86.org
  *
  * Permission to use, copy, modify, distribute, and sell this software and its
  * documentation for any purpose is hereby granted without fee, provided that
--- programs/Xserver/hw/xfree86/drivers/ati/atiendian.c
+++ programs/Xserver/hw/xfree86/drivers/ati/atiendian.c
@@ -1,6 +1,6 @@
-/* $XFree86: xc/programs/Xserver/hw/xfree86/drivers/ati/atiendian.c,v 1.3tsi Exp $ */
+/* $XFree86: xc/programs/Xserver/hw/xfree86/drivers/ati/atiendian.c,v 1.4tsi Exp $ */
 /*
- * Copyright 2005 by Marc Aurele La France (TSI @ UQV), tsi@xfree86.org
+ * Copyright 2005 through 2006 by Marc Aurele La France (TSI @ UQV), tsi@xfree86.org
  *
  * Permission to use, copy, modify, distribute, and sell this software and its
  * documentation for any purpose is hereby granted without fee, provided that
