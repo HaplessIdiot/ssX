74d2ce1d01aee77a3fa780107e6de82c9830e710
dawes <dawes>
2006-01-08 22:50:28 +0000

    179. Add Xasprintf to dummylib (David Dawes).

--- programs/Xserver/hw/xfree86/dummylib/xalloc.c
+++ programs/Xserver/hw/xfree86/dummylib/xalloc.c
@@ -1,4 +1,4 @@
-/* $XFree86: xc/programs/Xserver/hw/xfree86/dummylib/xalloc.c,v 1.3tsi Exp $ */
+/* $XFree86: xc/programs/Xserver/hw/xfree86/dummylib/xalloc.c,v 1.4 2005/10/14 15:16:50 tsi Exp $ */
 
 #include <X11/X.h>
 #include "os.h"
@@ -6,7 +6,7 @@
 #include "xf86Priv.h"
 
 /*
- * Utility functions required by libxf86_os. 
+ * Utility functions required by libxf86_os, the loader, and others. 
  */
 
 pointer
@@ -103,3 +103,50 @@ XNFstrdup(const char *s)
     strcpy(sd, s);
     return sd;
 }
+
+int
+Xasprintf(char **ret, const char *format, ...)
+{
+    char *s;
+    va_list args;
+    int status;
+
+    if (!ret || !format)
+	return -1;
+
+#ifdef HAS_ASPRINTF
+    va_start(args, format);
+    status = vasprintf(&s, format, args);
+    va_end(args);
+    if (status != -1 && s) {
+	*ret = Xstrdup(s);
+	free(s);
+	if (!*ret)
+	    status = -1;
+    } else
+	*ret = NULL;
+    return status;
+#else
+#define TMP_SIZE 4000
+    s = xcalloc(1, TMP_SIZE);
+    if (!s) {
+	*ret = NULL;
+	return -1;
+    }
+    va_start(args, format);
+    status = vsnprintf(s, TMP_SIZE, format, args);
+    va_end(args);
+    if (status > TMP_SIZE - 1)
+	status = TMP_SIZE - 1;
+    if (status < TMP_SIZE - 1) {
+	*ret = xrealloc(s, status + 1);
+	if (!*ret) {
+	    xfree(s);
+	    status = -1;
+	}
+    } else
+	*ret = s;
+    return status;
+#endif
+}
+
