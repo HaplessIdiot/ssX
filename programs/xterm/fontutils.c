34a471e22688e9fae1f094c3fb9038db0c1af189
dickey <dickey>
2006-04-30 21:55:38 +0000

    281. Xterm patch #213 (Thomas Dickey).

--- programs/xterm/fontutils.c
+++ programs/xterm/fontutils.c
@@ -1,7 +1,7 @@
-/* $XTermId: fontutils.c,v 1.200 2006/03/13 01:22:09 tom Exp $ */
+/* $XTermId: fontutils.c,v 1.202 2006/04/30 17:33:03 tom Exp $ */
 
 /*
- * $XFree86: xc/programs/xterm/fontutils.c,v 1.57 2006/02/13 01:14:58 dickey Exp $
+ * $XFree86: xc/programs/xterm/fontutils.c,v 1.59 2006/03/13 01:27:59 dickey Exp $
  */
 
 /************************************************************
@@ -1233,6 +1233,10 @@ xtermLoadWideFonts(XtermWidget xw, Bool nullOk)
     } else {
 	result = (isWideFont(screen->fnts[fWide], "wide", nullOk)
 		  && isWideFont(screen->fnts[fWBold], "wide-bold", nullOk));
+	if (result && !screen->utf8_latin1) {
+	    result = (isWideFont(screen->fnts[fNorm], "normal", nullOk)
+		      && isWideFont(screen->fnts[fBold], "bold", nullOk));
+	}
     }
     if (!result) {
 	TRACE(("current fonts are not all wide%s\n", nullOk ? " nullOk" : ""));
--- programs/xterm/version.h
+++ programs/xterm/version.h
@@ -1,5 +1,5 @@
-/* $XTermId: version.h,v 1.263 2006/04/10 00:19:08 tom Exp $ */
-/* $XFree86: xc/programs/xterm/version.h,v 3.122 2006/03/20 00:36:19 dickey Exp $ */
+/* $XTermId: version.h,v 1.265 2006/04/26 13:16:01 tom Exp $ */
+/* $XFree86: xc/programs/xterm/version.h,v 3.123 2006/04/10 00:34:37 dickey Exp $ */
 
 /*
  * These definitions are used to build the string that's printed in response to
@@ -7,7 +7,7 @@
  * version of X to which this version of xterm has been built.  The number in
  * parentheses is my patch number (T.Dickey).
  */
-#define XTERM_PATCH   212
+#define XTERM_PATCH   213
 
 #ifndef __vendorversion__
 #define __vendorversion__ "XTerm"
