7b6b5a3f4ed6269d0575e2cb4c0d672ab71b5059
tsi <tsi>
2006-12-11 23:17:40 +0000

    Typo

--- config/makedepend/main.c
+++ config/makedepend/main.c
@@ -23,7 +23,7 @@ used in advertising or otherwise to promote the sale, use or other dealings
 in this Software without prior written authorization from The Open Group.
 
 */
-/* $XFree86: xc/config/makedepend/main.c,v 3.35tsi Exp $ */
+/* $XFree86: xc/config/makedepend/main.c,v 3.36tsi Exp $ */
 
 #include "def.h"
 #ifdef hpux
@@ -708,7 +708,8 @@ char *getnextline(struct filepointer *filep)
 				 * TurboC seems to want its pragmas in column
 				 * 2.
 				 */
-				if (!strcmp(cp, "pragma")) whitespace = FALSE;
+				if (!strncmp(cp, "pragma", 6))
+					whitespace = FALSE;
 				if (*cp) goto done;
 				--p;
 			}
