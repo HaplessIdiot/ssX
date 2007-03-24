a18dd10e596cc52edc994709ca4c38b23aacad45
tsi <tsi>
2007-03-24 02:58:16 +0000

    123. Resurrect support for Ti3026 RAMDACs found on original I128 adapters.       8bpp still has incorrect colourmap entries and hardware cursor support       is still disabled  (Christos Zoulas).

--- programs/Xserver/hw/xfree86/drivers/i128/i128_driver.c
+++ programs/Xserver/hw/xfree86/drivers/i128/i128_driver.c
@@ -22,7 +22,7 @@
  *
  */
 
-/* $XFree86: xc/programs/Xserver/hw/xfree86/drivers/i128/i128_driver.c,v 1.39tsi Exp $ */
+/* $XFree86: xc/programs/Xserver/hw/xfree86/drivers/i128/i128_driver.c,v 1.40tsi Exp $ */
 
 
 /* All drivers should typically include these */
@@ -947,6 +947,7 @@ I128PreInit(ScrnInfoPtr pScrn, int flags)
 			mclk / 1000.0);
 	  pI128->minClock = 20000;
 	  pI128->ProgramDAC = I128ProgramTi3025;
+	  pI128->HWCursor = FALSE;
 	  break;
 
        case IBM524_DAC:
